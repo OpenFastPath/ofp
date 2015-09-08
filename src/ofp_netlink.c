OpenFastPath/ofp

commit fcc513ba98d33cf1a1ed21b94a673d67c371f7d3

Fix OFP_DEBUG compile-time define
This patch fixes the situation where OFP_DEBUG_PRINT was always passed
at compile-time regardless of whether --enable-debug was specified
during the configuration stage.

Now, -DOFP_DEBUG will be passed iff --enable-debug is specified. The
OFP_DBG(..) logging macro will now be compiled-out correctly.

Additionally, any code that is determined to be too much overhead for
a hot code path can be compiled out for a non-debug build:

  #if defined(OFP_DEBUG)
          // Additional debug checking code in a hot code path..
  #endif

Signed-off-by: Brian Brooks <brian.brooks@arm.com>

src/ofp_netlink.c
@@ -85,8 +85,9 @@ static int handle_ipv4v6_route(struct nlmsghdr *nlp)
 				dst6 = RTA_DATA(rtap);
 				dsts  = ofp_print_ip6_addr((uint8_t *)dst6);
 				OFP_DBG("Dest: %s", dsts);
-			} else
+			} else {
 				OFP_DBG("Unknown RTA_DST, len=%d", dst_len);
+			}
 
 			break;
 
@@ -266,7 +267,7 @@ static int handle_ipv4v6_addr(struct nlmsghdr *nlh)
 	/* msg RTM_NEWADDR / RTM_DELADDR contain an ifaddrmsg structure,
 	optionally followed by rtattr routing attributes */
 	struct ifaddrmsg *if_entry;
-	char if_address[32] , *if_addr = NULL;
+	char if_address[32];
 	char *name = NULL;
 	unsigned char *addr = NULL , *bcast = NULL, *laddr = NULL;
 	struct rtattr *rtap;
@@ -306,12 +307,11 @@ The processed msg here RTM_NEWADDR, RTM_DELADDR start with ifaddrmsg
 		case IFA_ADDRESS:
 			addr = RTA_DATA(rtap);
 			if (if_entry->ifa_family == AF_INET) {
-				if_addr = ofp_print_ip_addr(
-						*(uint32_t *)addr);
-				OFP_DBG("Addr = %s", if_addr);
+				OFP_DBG("Addr = %s",
+					ofp_print_ip_addr(*(uint32_t *)addr));
 			} else if (if_entry->ifa_family == AF_INET6) {
-				if_addr = ofp_print_ip6_addr(addr);
-				OFP_DBG("IP6 Addr = %s", if_addr);
+				OFP_DBG("IP6 Addr = %s",
+					ofp_print_ip6_addr(addr));
 			}
 			break;
 
@@ -355,7 +355,6 @@ The processed msg here RTM_NEWADDR, RTM_DELADDR start with ifaddrmsg
 			OFP_ERR("Local address not received for GRE interface");
 			return -1;
 		}
-		if_addr = ofp_print_ip_addr(*(uint32_t *)laddr);
 	}
 
 	if (!name)
@@ -363,7 +362,10 @@ The processed msg here RTM_NEWADDR, RTM_DELADDR start with ifaddrmsg
 
 	OFP_DBG("%s addr to ifx --> %s OIF %d name %s",
 		nlh->nlmsg_type == RTM_NEWADDR ? "Adding" : "Deleting",
-		if_addr, if_entry->ifa_index, name);
+		(if_entry->ifa_family == AF_INET) ?
+		ofp_print_ip_addr(*(uint32_t *)if_addr) :
+		ofp_print_ip6_addr(addr),
+		if_entry->ifa_index, name);
 
 	if (nlh->nlmsg_type == RTM_DELADDR)
 		return del_ipv4v6_addr(if_entry, dev, addr, laddr);

configure.ac
@@ -135,13 +135,13 @@ else
 fi
 
 # Enable/disable OFP_DEBUG_PRINT
-ODP_DEBUG=1
+ofp_debug=no
 AC_ARG_ENABLE([debug],
-    [  --enable-debug         Enable/disable ODP debug],
-    [if ! test "x$enableval" = "xyes"; then
-        ODP_DEBUG=0
-    fi])
-AM_CFLAGS="$AM_CFLAGS -DOFP_DEBUG_PRINT=$ODP_DEBUG"
+    [  --enable-debug         Enable OFP debug],
+    [ofp_debug=$enableval])
+if test "$ofp_debug" == "yes" ; then
+    AM_CPPFLAGS="$AM_CPPFLAGS -DOFP_DEBUG"
+fi
 
 # Enable/disable INET6 domain
 AC_ARG_ENABLE([ipv6],

include/api/ofp_log.h
@@ -22,28 +22,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef OFP_DEBUG_PRINT
-#define OFP_DEBUG_PRINT 1
+/*
+ * These logging macros can be used to send a message to the logging
+ * destination. Currently, this is stderr.
+ *
+ * Log line format:
+ *
+ *   L t coreid:threadid file:line] msg...
+ *
+ * where the fields are defined as:
+ *
+ *   L        - A single character, representing the log level
+ *   t        - ODP tick count
+ *   coreid   - ODP core id
+ *   threadid - pthread thread id
+ *   file     - The file name
+ *   line     - The line number
+ *   msg      - The user-supplied message
+ *
+ * Example:
+ *
+ *   I 1595 7:2172776256 ofp_pkt_processing.c:48] Bad checksum, dropping packet
+ *   D 1596 7:2172776256 ofp_pkt_processing.c:49] Begin ofp_tcp_input()
+ */
+#define OFP_LOG(fmt, ...) \
+	_ODP_FP_LOG(OFP_LOG_INFO, fmt, ##__VA_ARGS__)
+#define OFP_INFO(fmt, ...) \
+	_ODP_FP_LOG(OFP_LOG_INFO, fmt, ##__VA_ARGS__)
+#define OFP_ERR(fmt, ...) \
+	_ODP_FP_LOG(OFP_LOG_ERR, fmt, ##__VA_ARGS__)
+#define OFP_ABORT(fmt, ...) \
+	_ODP_FP_LOG(OFP_LOG_ABORT, fmt, ##__VA_ARGS__)
+
+/*
+ * Debug macros which will be compiled out when --enable-debug is not used.
+ */
+#if defined(OFP_DEBUG)
+#define OFP_DBG(fmt, ...) \
+	_ODP_FP_LOG(OFP_LOG_DBG, fmt, ##__VA_ARGS__)
+#define OFP_IS_LOGLEVEL_DEBUG() \
+	(ofp_loglevel == OFP_LOG_DBG ? 1 : 0)
+#else
+#define OFP_DBG(fmt, ...)
+#define OFP_IS_LOGLEVEL_DEBUG() 0
 #endif
 
 /**
- * log level.
+ * Print output to stderr
  */
-enum ofp_log_level_s {
-	OFP_LOG_ABORT = 1,
-	OFP_LOG_ERR,
-	OFP_LOG_INFO,
-	OFP_LOG_DBG
-};
+#define OFP_LOG_NO_CTX(level, fmt, ...) do {	\
+		if (level > ofp_loglevel)	\
+			break;			\
+		fprintf(stderr, fmt, ##__VA_ARGS__);	\
+	} while (0)
 
-extern enum ofp_log_level_s ofp_loglevel;
 
 #define __FILENAME__ \
 	(strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-/**
- * default LOG macro.
- */
+/* Internal macro, do not use. */
 #define _ODP_FP_LOG(level, fmt, ...) do {				\
 		if (level > ofp_loglevel)				\
 			break;						\
@@ -59,95 +96,14 @@ extern enum ofp_log_level_s ofp_loglevel;
 		if (level == OFP_LOG_ABORT)				\
 			abort();					\
 	} while (0)
-#if 0
-#define _ODP_FP_LOG(level, fmt, ...) \
-do { \
-	int _t = ofp_timer_ticks(0); \
-	if (level > ofp_loglevel) \
-		break; \
-	switch (level) { \
-	case OFP_LOG_ERR: \
-		fprintf(stderr, "[%d] %d.%02d %s:%d:%s():" fmt, \
-			 odp_cpu_id(), _t/100, _t%100, \
-			__FILE__, __LINE__, __func__, ##__VA_ARGS__); \
-		break; \
-	case OFP_LOG_DBG: \
-		fprintf(stderr, "[%d] %d.%02d %s:%d:%s():" fmt, \
-			odp_cpu_id(), _t/100, _t%100, \
-			 __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
-		break; \
-	case OFP_LOG_ABORT: \
-		fprintf(stderr, "[%d] %d.%02d %s:%d:%s(): " fmt, \
-			odp_cpu_id(), _t/100, _t%100, \
-			__FILE__, __LINE__, __func__, ##__VA_ARGS__); \
-		abort(); \
-		break; \
-	case OFP_LOG_INFO: \
-		fprintf(stderr, "[%d] %d.%02d %s:%d:%s(): " fmt, \
-			odp_cpu_id(), _t/100, _t%100, \
-			__FILE__, __LINE__, __func__, ##__VA_ARGS__); \
-		break; \
-	default: \
-		fprintf(stderr, "Unknown LOG level"); \
-		break;\
-	} \
-} while (0)
-#endif
-/**
- * Debug printing macro, which prints output when DEBUG flag is set.
- */
-#if (OFP_DEBUG_PRINT == 1)
-# define OFP_DBG(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_DBG, fmt, ##__VA_ARGS__)
-# define OFP_IS_LOGLEVEL_DEBUG() \
-		(ofp_loglevel == OFP_LOG_DBG ? 1 : 0)
-#else
-# define OFP_DBG(fmt, ...)
-# define OFP_IS_LOGLEVEL_DEBUG() 0
-#endif
 
-/**
- * Print output to stderr (file, line and function).
- */
-#define OFP_ERR(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_ERR, fmt, ##__VA_ARGS__)
+enum ofp_log_level_s {
+	OFP_LOG_ABORT = 1,
+	OFP_LOG_ERR,
+	OFP_LOG_INFO,
+	OFP_LOG_DBG
+};
 
-/**
- * Print output to stderr (file, line and function),
- * then abort.
- */
-#define OFP_ABORT(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_ABORT, fmt, ##__VA_ARGS__)
-
-/**
- * Print output to stderr (file, line and function)
- */
-#define OFP_LOG(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_INFO, fmt, ##__VA_ARGS__)
-
-/**
- * Print output to stderr (file, line and function)
- */
-#define OFP_INFO(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_INFO, fmt, ##__VA_ARGS__)
-
-/**
- * Print output to stderr
- */
-#define OFP_LOG_NO_CTX(level, fmt, ...) \
-do { \
-	if (level > ofp_loglevel) \
-		break; \
-	fprintf(stderr, fmt, ##__VA_ARGS__); \
-} while (0)
-
-/**
- * Intentionally unused variables to functions
- */
-#define OFP_UNUSED     __attribute__((__unused__))
-
-/**
- * @}
- */
+extern enum ofp_log_level_s ofp_loglevel;
 
 #endif /*__OFP_LOG_H__*/

src/ofp_icmp.c
@@ -499,6 +499,7 @@ reflect:
 
 	case OFP_ICMP_REDIRECT:
 		/*if (V_log_redirect)*/ {
+#if defined(OFP_DEBUG)
 			u_long src, dst, gw;
 
 			src = odp_be_to_cpu_32(ip->ip_src.s_addr);
@@ -512,6 +513,7 @@ reflect:
 			       (int)((dst >> 8) & 0xff), (int)(dst & 0xff),
 			       (int)(gw >> 24), (int)((gw >> 16) & 0xff),
 			       (int)((gw >> 8) & 0xff), (int)(gw & 0xff));
+#endif  /* OFP_DEBUG */
 		}
 		/*
 		 * RFC1812 says we must ignore ICMP redirects if we

src/ofp_route.c
@@ -157,6 +157,8 @@ int ofp_del_mac(struct ofp_ifnet *dev, uint32_t addr, uint8_t *mac)
 {
 	int ret;
 
+	(void) mac;
+
 	OFP_DBG("Port %d vlan %d vrf %d add_mac  ip %s - MAC %s" ,
 		dev->port, dev->vlan , dev->vrf ,
 		ofp_print_ip_addr(addr) , ofp_print_mac(mac));
@@ -184,6 +186,8 @@ void ofp_add_mac6(struct ofp_ifnet *dev, uint8_t *addr, uint8_t *mac)
 	struct ofp_nh6_entry *nh;
 	struct pkt6_entry *pktentry;
 	struct pkt6_list pkt6_send;
+
+	(void) dev;
 
 	OFP_LOCK_READ(route);
 	nh = ofp_rtl_search6(&shm->default_routes_6, addr);
@@ -275,11 +279,13 @@ static int del_route(struct ofp_route_msg *msg)
 			OFP_UNLOCK_WRITE(route);
 			return -1;
 		}
-		if (!ofp_rtl_remove(&(data->routes), msg->dst, msg->masklen))
+		if (!ofp_rtl_remove(&(data->routes), msg->dst, msg->masklen)) {
 			OFP_DBG("ofp_rtl_remove failed");
+		}
 	} else {
-		if (!ofp_rtl_remove(&shm->default_routes, msg->dst, msg->masklen))
+		if (!ofp_rtl_remove(&shm->default_routes, msg->dst, msg->masklen)) {
 			OFP_DBG("ofp_rtl_remove failed");
+		}
 	}
 #ifdef MTRIE
 	ofp_rt_rule_remove(msg->vrf, msg->dst, msg->masklen);
@@ -308,8 +314,9 @@ static int add_route6(struct ofp_route_msg *msg)
 		   ofp_print_ip6_addr(msg->gw6));
 
 	if (ofp_rtl_insert6(&shm->default_routes_6, msg->dst6,
-			msg->masklen, &tmp))
+			    msg->masklen, &tmp)) {
 		OFP_DBG("ofp_rtl_insert6 failed");
+	}
 
 	OFP_UNLOCK_WRITE(route);
 
@@ -334,8 +341,9 @@ static int del_route6(struct ofp_route_msg *msg)
 			odp_packet_free(pktentry->pkt);
 			pkt6_entry_free(pktentry);
 		}
-	} else
+	} else {
 		OFP_DBG("ofp_rtl_remove6 failed");
+	}
 
 	OFP_UNLOCK_WRITE(route);
 
@@ -464,8 +472,9 @@ static int add_local_interface(struct ofp_route_msg *msg)
 static int del_local_interface(struct ofp_route_msg *msg)
 {
 		OFP_LOCK_WRITE(route);
-		if (!ofp_rtl_remove(&shm->default_routes, msg->dst, 32))
+		if (!ofp_rtl_remove(&shm->default_routes, msg->dst, 32)) {
 			OFP_DBG("ofp_rtl_remove failed");
+		}
 		OFP_UNLOCK_WRITE(route);
 
 		return 0;

src/ofp_tcp_input.c
@@ -2042,8 +2042,9 @@ ofp_tcp_do_segment(odp_packet_t m, struct ofp_tcphdr *th, struct socket *so,
 			TCPSTAT_ADD(tcps_rcvdupbyte, tlen);
 			TCPSTAT_INC(tcps_pawsdrop);
 			if (tlen) {
-				if (V_tcp_passive_trace)
+				if (V_tcp_passive_trace) {
 					OFP_DBG("Drop after ACK (1)");
+				}
 				goto dropafterack;
 			}
 			goto drop;
@@ -2152,14 +2153,16 @@ ofp_tcp_do_segment(odp_packet_t m, struct ofp_tcphdr *th, struct socket *so,
 				t_flags_or(tp->t_flags, TF_ACKNOW);
 				TCPSTAT_INC(tcps_rcvwinprobe);
 			} else {
-				if (V_tcp_passive_trace)
+				if (V_tcp_passive_trace) {
 					OFP_DBG("Drop after ACK (2) wnd=%lu seq=%u next=%u",
 						tp->rcv_wnd, th->th_seq, tp->rcv_nxt);
+				}
 				goto dropafterack;
 			}
 		} else {
-			if (V_tcp_passive_trace)
+			if (V_tcp_passive_trace) {
 				OFP_DBG("Dropping %u bytes after window", todrop);
+			}
 			TCPSTAT_ADD(tcps_rcvbyteafterwin, todrop);
 		}
 		odp_packet_pull_tail(m, todrop);
@@ -2217,8 +2220,9 @@ ofp_tcp_do_segment(odp_packet_t m, struct ofp_tcphdr *th, struct socket *so,
 		    (tp->t_flags & TF_NEEDSYN))
 			goto step6;
 		else if (tp->t_flags & TF_ACKNOW) {
-			if (V_tcp_passive_trace)
+			if (V_tcp_passive_trace) {
 				OFP_DBG("Drop after ACK (3)");
+			}
 			goto dropafterack;
 		} else
 			goto drop;
@@ -2290,8 +2294,9 @@ ofp_tcp_do_segment(odp_packet_t m, struct ofp_tcphdr *th, struct socket *so,
 #endif
 		if (SEQ_GT(th->th_ack, tp->snd_max)) {
 			TCPSTAT_INC(tcps_rcvacktoomuch);
-			if (V_tcp_passive_trace)
+			if (V_tcp_passive_trace) {
 				OFP_DBG("Drop after ACK (4)");
+			}
 			goto dropafterack;
 		}
 		if ((tp->t_flags & TF_SACK_PERMIT) &&
@@ -2634,8 +2639,9 @@ process_ACK:
 				ofp_tcp_twstart(tp);
 				INP_INFO_WUNLOCK(&V_tcbinfo);
 				odp_packet_free(m);
-				if (V_tcp_passive_trace)
+				if (V_tcp_passive_trace) {
 					OFP_DBG("Closing ourfinisacked tlen=%u", tlen);
+				}
 				return;
 			}
 			break;
@@ -2934,8 +2940,9 @@ dropafterack:
 
 	if (V_tcp_passive_trace) {
 		OFP_DBG("Drop after ACK tlen=%d", tlen);
-		if (thflags & OFP_TH_FIN)
+		if (thflags & OFP_TH_FIN) {
 			OFP_DBG("Dropping FIN");
+		}
 	}
 	/*
 	 * Generate an ACK dropping incoming segment if it occupies
@@ -2978,8 +2985,9 @@ dropwithreset:
 
 	if (V_tcp_passive_trace) {
 		OFP_DBG("Drop with reset tlen=%d", tlen);
-		if (thflags & OFP_TH_FIN)
+		if (thflags & OFP_TH_FIN) {
 			OFP_DBG("Dropping FIN (2)");
+		}
 	}
 	if (ti_locked == TI_WLOCKED)
 		INP_INFO_WUNLOCK(&V_tcbinfo);
@@ -3002,8 +3010,9 @@ drop:
 
 	if (V_tcp_passive_trace) {
 		OFP_DBG("Drop tlen=%d", tlen);
-		if (thflags & OFP_TH_FIN)
+		if (thflags & OFP_TH_FIN) {
 			OFP_DBG("Dropping FIN (3)");
+		}
 	}
 	if (ti_locked == TI_WLOCKED) {
 		INP_INFO_WUNLOCK(&V_tcbinfo);

src/ofp_tcp_syncache.c
@@ -437,9 +437,10 @@ ofp_syncache_chkrst(struct in_conninfo *inc, struct ofp_tcphdr *th)
 	 * See RFC 793 page 65, section SEGMENT ARRIVES.
 	 */
 	if (th->th_flags & (OFP_TH_ACK|OFP_TH_SYN|OFP_TH_FIN)) {
-		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL)))
+		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL))) {
 			OFP_DBG("%s: Spurious RST with ACK, SYN or "
 			    "FIN flag set, segment ignored", s);
+		}
 		TCPSTAT_INC(tcps_badrst);
 		goto done;
 	}
@@ -453,10 +454,11 @@ ofp_syncache_chkrst(struct in_conninfo *inc, struct ofp_tcphdr *th)
 	 * Otherwise the RST was misdirected or spoofed.
 	 */
 	if (sc == NULL) {
-		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL)))
+		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL))) {
 			OFP_DBG("%s: Spurious RST without matching "
 			    "syncache entry (possibly syncookie only), "
 			    "segment ignored", s);
+		}
 		TCPSTAT_INC(tcps_badrst);
 		goto done;
 	}
@@ -477,16 +479,18 @@ ofp_syncache_chkrst(struct in_conninfo *inc, struct ofp_tcphdr *th)
 	if (SEQ_GEQ(th->th_seq, sc->sc_irs) &&
 	    SEQ_LEQ(th->th_seq, sc->sc_irs + sc->sc_wnd)) {
 		syncache_drop(sc, sch);
-		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL)))
+		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL))) {
 			OFP_DBG("%s: Our SYN|ACK was rejected, "
 				"connection attempt aborted by remote endpoint",
 				s);
+		}
 		TCPSTAT_INC(tcps_sc_reset);
 	} else {
-		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL)))
+		if ((s = ofp_tcp_log_addrs(inc, th, NULL, NULL))) {
 			OFP_DBG("%s: RST with invalid SEQ %u != "
 			    "IRS %u (+WND %u), segment ignored",
 			    s, th->th_seq, sc->sc_irs, sc->sc_wnd);
+		}
 		TCPSTAT_INC(tcps_badrst);
 	}
 

commit 559831550e3d2bf0ca5edcc4c28bcd51a89cac07

Fix build breakage introduced in fcc513ba98
Signed-off-by: Brian Brooks <brian.brooks@arm.com>

src/ofp_netlink.c
@@ -363,7 +363,7 @@ The processed msg here RTM_NEWADDR, RTM_DELADDR start with ifaddrmsg
 	OFP_DBG("%s addr to ifx --> %s OIF %d name %s",
 		nlh->nlmsg_type == RTM_NEWADDR ? "Adding" : "Deleting",
 		(if_entry->ifa_family == AF_INET) ?
-		ofp_print_ip_addr(*(uint32_t *)if_addr) :
+		ofp_print_ip_addr(*(uint32_t *)addr) :
 		ofp_print_ip6_addr(addr),
 		if_entry->ifa_index, name);
 