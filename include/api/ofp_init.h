6a85fb6a09b57ae8fba99f46791fd1d77275a10a
Sorin Vultureanu <sorin.vultureanu@enea.com>
2015-10-15 15:41:26 +0300

    Init API documentation
    Signed-off-by: Sorin Vultureanu <sorin.vultureanu@enea.com>
    Reviewed-by: Jose Pekkarinen <jose.pekkarinen@nokia.com>

--- include/api/ofp_init.h
+++ include/api/ofp_init.h
@@ -5,20 +5,66 @@
  * SPDX-License-Identifier:	BSD-3-Clause
  */
 
+/**
+ * @file
+ *
+ * @brief OFP initialization.
+ *
+ * OFP requires a global level init for the API library and a local init per
+ * thread before the other OFP APIs may be called.
+ * - ofp_init_global()
+ * - ofp_init_local()
+ *
+ * For a graceful termination the matching termination APIs exit
+ * - ofp_term_global()
+ * - ofp_term_local()
+ */
+
 #ifndef __OFP_INIT_H__
 #define __OFP_INIT_H__
 
 #include "ofp_hook.h"
 
+/**
+ * OFP API initialization data
+ */
 typedef struct ofp_init_global_t {
-	uint16_t if_count;
-	uint16_t linux_core_id;
-	char **if_names;
-	ofp_pkt_hook pkt_hook[OFP_HOOK_MAX];
-	uint8_t burst_recv_mode;
+	uint16_t if_count;	/**< Interface count that needs init */
+	uint16_t linux_core_id;	/**< Core index that is reserved for Linux */
+	char **if_names;	/**< Interface names in relation to #if_count */
+	ofp_pkt_hook pkt_hook[OFP_HOOK_MAX];	/**< @see ofp_hook.h */
+	uint8_t burst_recv_mode;		/**< Interfaces use polling
+						when value set is 1.
+						Interfaces use scheduling
+						when value set is 0. */
 } ofp_init_global_t;
 
+/**
+ * OFP global initialization
+ *
+ * This function must be called once in an ODP control thread before calling any
+ * other OFP API functions.
+ *
+ * @param params Structure with parameters for global init of OFP API
+ *
+ * @retval 0 on success
+ * @retval -1 on failure
+ *
+ * @see ofp_init_local() which is required per thread before use.
+ */
 int ofp_init_global(ofp_init_global_t *params);
+
+/**
+ * Thread local OFP initialization
+ *
+ * All threads must call this function before calling any other OFP API
+ * functions.
+ *
+ * @retval 0 on success
+ * @retval -1 on failure
+ *
+ * @see ofp_init_global() which must have been called prior to this.
+ */
 int ofp_init_local(void);
 
 #endif /* __OFP_INIT_H__ */
