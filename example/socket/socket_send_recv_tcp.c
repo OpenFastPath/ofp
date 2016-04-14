095b57ac510cf17abad1f6096bf04f2d67fda3a6
Sorin Vultureanu <Sorin.Vultureanu@enea.com>
2016-04-14 14:17:36 +0300

    Improve socket example to check incoming data
    Signed-off-by: Sorin Vultureanu <Sorin.Vultureanu@enea.com>
    Reviewed-by: José Pekkarinen <jose.pekkarinen@nokia.com>

--- example/socket/socket_send_recv_tcp.c
+++ example/socket/socket_send_recv_tcp.c
@@ -1,5 +1,5 @@
-/* Copyright (c) 2014, ENEA Software AB
- * Copyright (c) 2014, Nokia
+/* Copyright (c) 2016, ENEA Software AB
+ * Copyright (c) 2016, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier:	BSD-3-Clause
@@ -9,10 +9,12 @@
 #include "socket_send_recv_tcp.h"
 #include "socket_util.h"
 
+#define TCP_CYCLES 20
+static const char *tcp_buf = "1234567890 2345678901 3456789012 4567890123 5678901234 6789012345 7890123456 socket_test";
+
 int send_tcp4_local_ip(int fd)
 {
 	struct ofp_sockaddr_in addr = {0};
-	const char *buf = "socket_test";
 
 	addr.sin_len = sizeof(struct ofp_sockaddr_in);
 	addr.sin_family = OFP_AF_INET;
@@ -27,7 +29,7 @@ int send_tcp4_local_ip(int fd)
 
 	sleep(1); /* ToFix: connect is not blocking*/
 
-	if (ofp_send(fd, buf, strlen(buf) + 1, 0) == -1) {
+	if (ofp_send(fd, tcp_buf, strlen(tcp_buf) + 1, 0) == -1) {
 		OFP_ERR("Faild to send (errno = %d)\n", ofp_errno);
 		return -1;
 	}
@@ -39,7 +41,6 @@ int send_tcp4_local_ip(int fd)
 int send_tcp4_any(int fd)
 {
 	struct ofp_sockaddr_in addr = {0};
-	const char *buf = "socket_test";
 
 	addr.sin_len = sizeof(struct ofp_sockaddr_in);
 	addr.sin_family = OFP_AF_INET;
@@ -54,7 +55,7 @@ int send_tcp4_any(int fd)
 
 	sleep(1); /* ToFix: connect is not blocking*/
 
-	if (ofp_send(fd, buf, strlen(buf) + 1, 0) == -1) {
+	if (ofp_send(fd, tcp_buf, strlen(tcp_buf) + 1, 0) == -1) {
 		OFP_ERR("Faild to send (errno = %d)\n", ofp_errno);
 		return -1;
 	}
@@ -67,7 +68,6 @@ int send_tcp4_any(int fd)
 int send_tcp6_local_ip(int fd)
 {
 	struct ofp_sockaddr_in6 addr = {0};
-	const char *buf = "socket_test";
 
 	addr.sin6_len = sizeof(struct ofp_sockaddr_in6);
 	addr.sin6_family = OFP_AF_INET6;
@@ -82,7 +82,7 @@ int send_tcp6_local_ip(int fd)
 
 	sleep(1); /* ToFix: connect is not blocking*/
 
-	if (ofp_send(fd, buf, strlen(buf) + 1, 0) == -1) {
+	if (ofp_send(fd, tcp_buf, strlen(tcp_buf) + 1, 0) == -1) {
 		OFP_ERR("Faild to send (errno = %d)\n", ofp_errno);
 		return -1;
 	}
@@ -94,7 +94,6 @@ int send_tcp6_local_ip(int fd)
 int send_tcp6_any(int fd)
 {
 	struct ofp_sockaddr_in6 addr = {0};
-	const char *buf = "socket_test";
 
 	addr.sin6_len = sizeof(struct ofp_sockaddr_in6);
 	addr.sin6_family = OFP_AF_INET6;
@@ -109,7 +108,7 @@ int send_tcp6_any(int fd)
 
 	sleep(1); /* ToFix: connect is not blocking*/
 
-	if (ofp_send(fd, buf, strlen(buf) + 1, 0) == -1) {
+	if (ofp_send(fd, tcp_buf, strlen(tcp_buf) + 1, 0) == -1) {
 		OFP_ERR("Faild to send (errno = %d)\n", ofp_errno);
 		return -1;
 	}
@@ -121,30 +120,40 @@ int send_tcp6_any(int fd)
 
 int receive_tcp(int fd)
 {
-	char buf[20];
+	char buf[1024];
 	int len = sizeof(buf);
 	int fd_accepted = -1;
 
 	fd_accepted = ofp_accept(fd, NULL, NULL);
 
 	if (fd_accepted == -1) {
-		OFP_ERR("Faild to accept connection (errno = %d)\n",
+		OFP_ERR("FAILED to accept connection (errno = %d)\n",
 			ofp_errno);
 		return -1;
 	}
 
 	len = ofp_recv(fd_accepted, buf, sizeof(buf), 0);
 	if (len == -1) {
-		OFP_ERR("Faild to recv (errno = %d)\n",
+		OFP_ERR("FAILED to recv (errno = %d)\n",
 			ofp_errno);
-		ofp_close(fd_accepted);
+		ofp_close(fd_accepted);		return -1;
 		return -1;
 	}
 	buf[len] = 0;
-	OFP_INFO("Data (%s, len = %d) was received.\n", buf, len);
+	OFP_INFO("Data was received.\n");
+
+	if ((size_t)len != strlen(tcp_buf) + 1) {
+		OFP_ERR("FAILED : length received is wrong:[%d]\n", len);
+		return -1;
+	}
+
+	if (strcmp(buf, tcp_buf) != 0) {
+		OFP_ERR("FAILED : data received is malformed:[%s]\n", buf);
+		return -1;
+	}
 
 	if (ofp_close(fd_accepted) == -1) {
-		OFP_ERR("Faild to close accepted socket (errno = %d)\n",
+		OFP_ERR("FAILED to close accepted socket (errno = %d)\n",
 			ofp_errno);
 		return -1;
 	}
