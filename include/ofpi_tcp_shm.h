2eac673219c85ee8161e85a6d4cdd3170d0091a9
Sorin Vultureanu <sorin.vultureanu@enea.com>
2016-06-10 18:16:36 +0300

    TCP: ofp_tcp_var_mem shm: V_tcb, V_tcbinfo and V_twq_2msl, per core
    Enabled when #ifdef OFP_RSS
    
    Signed-off-by: Sorin Vultureanu <sorin.vultureanu@enea.com>
    Reviewed-by: Antero Nousiainen <antero.nousiainen@nokia.com>

--- include/ofpi_tcp_shm.h
+++ include/ofpi_tcp_shm.h
@@ -16,9 +16,15 @@
  * Shared data format
  */
 struct ofp_tcp_var_mem {
+#ifdef OFP_RSS
+	VNET_DEFINE(struct inpcbhead, ofp_tcb[OFP_MAX_NUM_CPU]);
+	VNET_DEFINE(struct inpcbinfo, ofp_tcbinfo[OFP_MAX_NUM_CPU]);
+	VNET_DEFINE(OFP_TAILQ_HEAD(, tcptw), twq_2msl[OFP_MAX_NUM_CPU]);
+#else
 	VNET_DEFINE(struct inpcbhead, ofp_tcb);/* queue of active tcpcb's */
 	VNET_DEFINE(struct inpcbinfo, ofp_tcbinfo);
 	VNET_DEFINE(OFP_TAILQ_HEAD(, tcptw), twq_2msl);
+#endif
 
 /* Target size of TCP PCB hash tables. Must be a power of two.*/
 #define TCBHASHSIZE			1024
@@ -36,11 +42,18 @@ struct ofp_tcp_var_mem {
 };
 extern __thread struct ofp_tcp_var_mem *shm_tcp;
 
+#ifdef OFP_RSS
+#define	V_tcb			VNET(shm_tcp->ofp_tcb[odp_cpu_id()])
+#define	V_tcbinfo		VNET(shm_tcp->ofp_tcbinfo[odp_cpu_id()])
+#define	V_twq_2msl		VNET(shm_tcp->twq_2msl[odp_cpu_id()])
+#else
 #define	V_tcb			VNET(shm_tcp->ofp_tcb)
 #define	V_tcbinfo		VNET(shm_tcp->ofp_tcbinfo)
+#define	V_twq_2msl		VNET(shm_tcp->twq_2msl)
+#endif
+
 #define	V_tcp_reass_zone	VNET(shm_tcp->tcp_reass_zone)
 #define	V_tcpcb_zone		VNET(shm_tcp->tcpcb_zone)
 #define	V_tcptw_zone		VNET(shm_tcp->tcptw_zone)
-#define	V_twq_2msl		VNET(shm_tcp->twq_2msl)
 #define	V_tcp_syncache_zone	VNET(shm_tcp->tcp_syncache_zone)
 #define	V_sack_hole_zone	VNET(shm_tcp->ofp_sack_hole_zone)
