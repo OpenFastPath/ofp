52e6a4ceefa704d246f9349d4e5dfe974877204b
Sorin Vultureanu <sorin.vultureanu@enea.com>
2016-09-01 16:02:14 +0200

    Update DPDK build script with newer ODP-DPDK and DPDK 16.07
    Signed-off-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

--- scripts/devbuild_ofp_odp_dpdk.sh
+++ scripts/devbuild_ofp_odp_dpdk.sh
@@ -9,7 +9,7 @@ cd ${REPOS}
 rm -Rf ./check-odp/
 git clone https://git.linaro.org/lng/check-odp.git
 pushd check-odp
-git checkout 9afc6e939c7de3ff781c742841a5b0bc3a345a52
+git checkout 6a957e933443eaa088eaf8565865eebc321e4700
 popd
 
 
@@ -17,14 +17,13 @@ popd
 rm -Rf ./odp-dpdk/
 git clone https://git.linaro.org/lng/odp-dpdk.git
 pushd odp-dpdk
-git checkout 0ed1ced007d98980f90604675083bf30c354e867
+git checkout 7068593f600e2b5a23ee1780d5c722c54e966df1
 popd
 
 # Clone/build DPDK
 echo '#include "pcap.h"' | cpp -H -o /dev/null 2>&1
 if [ "$?" != "0" ]; then
-    echo "Error: pcap is not installed. You may need to install libpcap-dev package."
-    exit 1
+    echo "Error: pcap.h is not detected. You may need to install libpcap-dev package."
 fi
 
 rm -Rf ./dpdk/
@@ -35,7 +34,7 @@ if [ "$?" != "0" ]; then
 fi
 
 # Build ODP-DPDK
-export CONFIGURE_FLAGS="--enable-debug --enable-debug-print --enable-cunit-support --enable-test-vald --enable-shared=yes"
+export CONFIGURE_FLAGS="--enable-shared=yes"
 odp-dpdk/scripts/devbuild.sh odp
 if [ "$?" != "0" ]; then
     echo "Instaling dpdk failed"
@@ -44,7 +43,7 @@ fi
 
 pushd ${ROOT_DIR}/..
 ./bootstrap
-./configure --with-odp=$REPOS/check-odp/new-build --enable-cunit --enable-debug --prefix=$REPOS/check-odp/new-build --with-odp-lib=odp-dpdk
+./configure --with-odp=$REPOS/check-odp/new-build --enable-cunit --prefix=$REPOS/check-odp/new-build --with-odp-lib=odp-dpdk
 make clean
 make
 make install
