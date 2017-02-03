37ef7df6cb54f6a3f21db115d3df1467015d89fc
Bogdan Pricope <bogdan.pricope@linaro.org>
2017-02-03 09:59:53 +0200

    examples: modify fpm_burstmode application to process timers in workers loop This will permit running application on odp platforms with only one pktin/pktout (odp-linux, default pktio)
    Signed-off-by: Bogdan Pricope <bogdan.pricope@linaro.org>
    Reviewed-by: Brian Brooks <brian.brooks@linaro.org>

--- example/fpm_burstmode/app_main.c
+++ example/fpm_burstmode/app_main.c
@@ -18,20 +18,24 @@
  */
 typedef struct {
 	int core_count;
+	int core_start;
 	int if_count;		/**< Number of interfaces to be used */
 	char **if_names;	/**< Array of pointers to interface names */
 	char *conf_file;
 } appl_args_t;
 
-struct pktio_thr_arg {
+struct worker_arg {
 	int num_pktin;
 	odp_pktin_queue_t pktin[OFP_FP_INTERFACE_MAX];
+	odp_bool_t process_timers;
 };
 
 /* helper funcs */
 static void parse_args(int argc, char *argv[], appl_args_t *appl_args);
 static void print_info(char *progname, appl_args_t *appl_args);
 static void usage(char *progname);
+static int validate_cores_settings(int req_core_start, int req_core_count,
+	int *core_start, int *core_count);
 
 ofp_init_global_t app_init_params; /**< global OFP init parms */
 
@@ -42,20 +46,29 @@ ofp_init_global_t app_init_params; /**< global OFP init parms */
 
 #define PKT_BURST_SIZE OFP_PKT_RX_BURST_SIZE
 
-static void *pkt_io_recv(void *arg)
+/** pkt_io_recv() Custom event dispatcher
+ *
+ * @param _arg void*  Worker argument
+ * @return void* Never returns
+ *
+ */
+static void *pkt_io_recv(void *_arg)
 {
 	odp_packet_t pkt, pkt_tbl[PKT_BURST_SIZE];
-	int pkt_idx, pkt_cnt;
-	struct pktio_thr_arg *thr_args;
+	odp_event_t events[PKT_BURST_SIZE], ev;
+	int pkt_idx, pkt_cnt, event_cnt;
+	struct worker_arg *arg;
 	int num_pktin, i;
 	odp_pktin_queue_t pktin[OFP_FP_INTERFACE_MAX];
 	uint8_t *ptr;
+	odp_bool_t process_timers;
 
-	thr_args = arg;
-	num_pktin = thr_args->num_pktin;
+	arg = (struct worker_arg *)_arg;
+	process_timers = arg->process_timers;
+	num_pktin = arg->num_pktin;
 
 	for (i = 0; i < num_pktin; i++)
-		pktin[i] = thr_args->pktin[i];
+		pktin[i] = arg->pktin[i];
 
 	if (ofp_init_local()) {
 		OFP_ERR("Error: OFP local init failed.\n");
@@ -67,6 +80,22 @@ static void *pkt_io_recv(void *arg)
 	       num_pktin, ptr[0], ptr[8]);
 
 	while (1) {
+		if (process_timers) {
+			event_cnt = odp_schedule_multi(NULL, ODP_SCHED_NO_WAIT,
+				events, PKT_BURST_SIZE);
+			for (i = 0; i < event_cnt; i++) {
+				ev = events[i];
+
+				if (ev == ODP_EVENT_INVALID)
+					continue;
+
+				if (odp_event_type(ev) == ODP_EVENT_TIMEOUT)
+					ofp_timer_handle(ev);
+				else
+					odp_buffer_free(
+						odp_buffer_from_event(ev));
+			}
+		}
 		for (i = 0; i < num_pktin; i++) {
 			pkt_cnt = odp_pktin_recv(pktin[i], pkt_tbl,
 						 PKT_BURST_SIZE);
@@ -85,39 +114,95 @@ static void *pkt_io_recv(void *arg)
 	return NULL;
 }
 
-/*
- * Should receive timeouts only
+/** configure_interfaces() Create OFP interfaces with
+ * pktios open in direct mode, thread unsafe.
+ *
+ * @param if_count int  Interface count
+ * @param if_names char** Interface names
+ * @param tx_queue int Number of requested transmision queues
+ *    per interface
+ * @param rx_queue int Number of requested reciver queues per
+ *    interface
+ * @return int 0 on success, -1 on error
+ *
  */
-static void *event_dispatcher(void *arg)
+static int configure_interfaces(odp_instance_t instance,
+	int if_count, char **if_names,
+	int tx_queues, int rx_queues)
 {
-	odp_event_t ev;
+	odp_pktio_param_t pktio_param;
+	odp_pktin_queue_param_t pktin_param;
+	odp_pktout_queue_param_t pktout_param;
+	int i;
 
-	(void)arg;
+	odp_pktio_param_init(&pktio_param);
+	pktio_param.in_mode = ODP_PKTIN_MODE_DIRECT;
+	pktio_param.out_mode = ODP_PKTOUT_MODE_DIRECT;
 
-	if (ofp_init_local()) {
-		OFP_ERR("Error: OFP local init failed.\n");
-		return NULL;
-	}
+	odp_pktin_queue_param_init(&pktin_param);
+	pktin_param.op_mode = ODP_PKTIO_OP_MT_UNSAFE;
+	pktin_param.hash_enable = 0;
+	pktin_param.num_queues = rx_queues;
 
-	while (1) {
-		ev = odp_schedule(NULL, ODP_SCHED_WAIT);
+	odp_pktout_queue_param_init(&pktout_param);
+	pktout_param.op_mode    = ODP_PKTIO_OP_MT_UNSAFE;
+	pktout_param.num_queues = tx_queues;
 
-		if (ev == ODP_EVENT_INVALID)
-			continue;
-
-		if (odp_event_type(ev) == ODP_EVENT_TIMEOUT) {
-			ofp_timer_handle(ev);
-			continue;
+	for (i = 0; i < if_count; i++)
+		if (ofp_ifnet_create(instance, if_names[i],
+				&pktio_param,
+				&pktin_param,
+				&pktout_param) < 0) {
+			OFP_ERR("Failed to init interface %s", if_names[i]);
+			return -1;
 		}
 
-		OFP_ERR("Error: unexpected event type: %u\n",
-			  odp_event_type(ev));
+	return 0;
+}
 
-		odp_buffer_free(odp_buffer_from_event(ev));
+/** configure_workers_arg() Configure workers
+ *  argument
+ *
+ * @param num_workers int  Number of workers
+ * @param workers_arg struct worker_arg* Array of workers
+ *    argument
+ * @param if_count int  Interface count
+ * @param if_names char** Interface names
+ * @return int 0 on success, -1 on error
+ *
+ */
+static int configure_workers_arg(int num_workers,
+	struct worker_arg *workers_arg,
+	int if_count, char **if_names)
+{
+	odp_pktio_t pktio;
+	odp_pktin_queue_t pktin[MAX_WORKERS];
+	int i,j;
+
+	for (i = 0; i < num_workers; i++) {
+		workers_arg[i].num_pktin = if_count;
+		workers_arg[i].process_timers = 0;
+	}
+	/*enable timer processing on first core*/
+	workers_arg[0].process_timers = 1;
+
+	for (i = 0; i < if_count; i++) {
+		pktio = odp_pktio_lookup(if_names[i]);
+		if (pktio == ODP_PKTIO_INVALID) {
+			OFP_ERR("Failed locate pktio %s", if_names[i]);
+			return -1;
+		}
+
+		if (odp_pktin_queue(pktio, pktin, num_workers) != num_workers) {
+			OFP_ERR("Too few pktin queues for %s", if_names[i]);
+			exit(EXIT_FAILURE);
+		}
+
+		for (j = 0; j < num_workers; j++)
+			workers_arg[j].pktin[i] = pktin[j];
 	}
 
-	/* Never reached */
-	return NULL;
+	return 0;
 }
 
 /** main() Application entry point
@@ -129,15 +214,11 @@ static void *event_dispatcher(void *arg)
  */
 int main(int argc, char *argv[])
 {
-	odph_linux_pthread_t thread_tbl[MAX_WORKERS], dispatcher_thread;
+	odph_linux_pthread_t thread_tbl[MAX_WORKERS];
+	struct worker_arg workers_arg[MAX_WORKERS];
 	appl_args_t params;
-	int num_workers, tx_queues, first_worker, i;
+	int num_workers, first_worker, linux_sp_core, i;
 	odp_cpumask_t cpu_mask;
-	struct pktio_thr_arg pktio_thr_args[MAX_WORKERS];
-	odp_pktio_param_t pktio_param;
-	odp_pktin_queue_param_t pktin_param;
-	odp_pktout_queue_param_t pktout_param;
-	odp_pktio_t pktio;
 	odph_linux_thr_params_t thr_params;
 	odp_instance_t instance;
 
@@ -163,28 +244,25 @@ int main(int argc, char *argv[])
 	print_info(NO_PATH(argv[0]), &params);
 
 	/*
-	 * By default core #0 runs Linux kernel background tasks. Start mapping
-	 * worker threads from core #1. Core #0 requires its own TX queue.
+	 * By default core #0 runs Slow Path background tasks.
+	 * It is recommanded to start mapping threads from core 1. Else,
+	 * Slow Path processing will be affected by workers processing.
+	 * However, if Slow Path is disabled, core 0 may be used as well.
 	 */
-	first_worker = 1;
-	num_workers = odp_cpu_count() - 1;
-
-	if (params.core_count && params.core_count < num_workers)
-		num_workers = params.core_count;
-	if (num_workers > MAX_WORKERS)
-		num_workers = MAX_WORKERS;
-	tx_queues = num_workers + 1;
-
-	if (num_workers < 1) {
-		OFP_ERR("ERROR: At least 2 cores required.\n");
+	if (validate_cores_settings(params.core_start, params.core_count,
+		&first_worker, &num_workers) < 0) {
+		odp_term_local();
+		odp_term_global(instance);
 		exit(EXIT_FAILURE);
 	}
+	linux_sp_core = 0;
+	OFP_INFO("SP core: %d\nWorkers core start: %d\n"
+		"Workers core count: %d\n",
+		linux_sp_core, first_worker, num_workers);
 
-	printf("Num worker threads: %i\n", num_workers);
-	printf("First worker CPU:   %i\n\n", first_worker);
-
+	/* Initialize OFP*/
 	memset(&app_init_params, 0, sizeof(app_init_params));
-	app_init_params.linux_core_id = 0;
+	app_init_params.linux_core_id = linux_sp_core;
 
 	if (ofp_init_global(instance, &app_init_params)) {
 		OFP_ERR("Error: OFP global init failed.\n");
@@ -195,67 +273,25 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	odp_pktio_param_init(&pktio_param);
-	pktio_param.in_mode = ODP_PKTIN_MODE_DIRECT;
-	pktio_param.out_mode = ODP_PKTOUT_MODE_DIRECT;
+	if (configure_interfaces(instance,
+		params.if_count, params.if_names,
+		num_workers, num_workers)) {
+		OFP_ERR("Error: Failed to configure interfaces.\n");
+		exit(EXIT_FAILURE);
+	}
 
-	odp_pktin_queue_param_init(&pktin_param);
-	pktin_param.op_mode = ODP_PKTIO_OP_MT_UNSAFE;
-	pktin_param.hash_enable = 1;
-	pktin_param.hash_proto.proto.ipv4_udp = 1;
-	pktin_param.num_queues = num_workers;
-
-	odp_pktout_queue_param_init(&pktout_param);
-	pktout_param.op_mode    = ODP_PKTIO_OP_MT_UNSAFE;
-	pktout_param.num_queues = tx_queues;
-
-	memset(pktio_thr_args, 0, sizeof(pktio_thr_args));
-
-	for (i = 0; i < params.if_count; i++) {
-		int j;
-		odp_pktin_queue_t pktin[num_workers];
-
-		if (ofp_ifnet_create(instance, params.if_names[i],
-				&pktio_param,
-				&pktin_param,
-				&pktout_param) < 0) {
-			OFP_ERR("Failed to init interface %s",
-				params.if_names[i]);
-			exit(EXIT_FAILURE);
-		}
-
-		pktio = odp_pktio_lookup(params.if_names[i]);
-		if (pktio == ODP_PKTIO_INVALID) {
-			OFP_ERR("Failed locate pktio %s",
-				params.if_names[i]);
-			exit(EXIT_FAILURE);
-		}
-
-		if (odp_pktin_queue(pktio, pktin, num_workers) != num_workers) {
-			OFP_ERR("Too few pktin queues for %s",
-				params.if_names[i]);
-			exit(EXIT_FAILURE);
-		}
-
-		if (odp_pktout_queue(pktio, NULL, 0) != tx_queues) {
-			OFP_ERR("Too few pktout queues for %s",
-				params.if_names[i]);
-			exit(EXIT_FAILURE);
-		}
-
-		for (j = 0; j < num_workers; j++)
-			pktio_thr_args[j].pktin[i] = pktin[j];
+	if (configure_workers_arg(num_workers, workers_arg,
+		params.if_count, params.if_names)) {
+		OFP_ERR("Failed to initialize workers arguments.");
+		exit(EXIT_FAILURE);
 	}
 
 	memset(thread_tbl, 0, sizeof(thread_tbl));
 
 	/* Create worker threads */
 	for (i = 0; i < num_workers; ++i) {
-
-		pktio_thr_args[i].num_pktin = params.if_count;
-
 		thr_params.start = pkt_io_recv;
-		thr_params.arg = &pktio_thr_args[i];
+		thr_params.arg = &workers_arg[i];
 		thr_params.thr_type = ODP_THREAD_WORKER;
 		thr_params.instance = instance;
 
@@ -266,16 +302,6 @@ int main(int argc, char *argv[])
 					  &thr_params);
 	}
 
-	odp_cpumask_zero(&cpu_mask);
-	odp_cpumask_set(&cpu_mask, app_init_params.linux_core_id);
-	thr_params.start = event_dispatcher;
-	thr_params.arg = NULL;
-	thr_params.thr_type = ODP_THREAD_WORKER;
-	thr_params.instance = instance;
-	odph_linux_pthread_create(&dispatcher_thread,
-				  &cpu_mask,
-				  &thr_params);
-
 	/* Start CLI */
 	ofp_start_cli_thread(instance, app_init_params.linux_core_id,
 			     params.conf_file);
@@ -283,6 +309,55 @@ int main(int argc, char *argv[])
 	odph_linux_pthread_join(thread_tbl, num_workers);
 	printf("End Main()\n");
 
+	return 0;
+}
+
+/**
+ * validate_cores_settings() Validate requested core settings
+ * and computed actual values
+ *
+ *
+ * @param req_core_start int Requested worker core start
+ * @param req_core_count int Requested worker core count
+ * @param core_start int* Computed worker core start
+ * @param core_count int* Computed worker core count
+ * @return int 0 on success, -1 on error
+ *
+ */
+static int validate_cores_settings(int req_core_start, int req_core_count,
+	 int *core_start, int *core_count)
+{
+	int total_core_count = odp_cpu_count();
+
+	if (req_core_start >= total_core_count) {
+		OFP_ERR("ERROR: Invalid 'core start' parameter: %d. Max = %d\n",
+			req_core_start, total_core_count - 1);
+		return -1;
+	}
+	*core_start = req_core_start;
+
+	if (req_core_count) {
+		if (*core_start + req_core_count > total_core_count) {
+			OFP_ERR("ERROR: Invalid 'core start' 'core count' "
+				"configuration: %d,%d\n"
+				"Exeeds number of avilable cores: %d",
+				*core_start, req_core_count, total_core_count);
+			return -1;
+		}
+		*core_count = req_core_count;
+	} else
+		*core_count = total_core_count - *core_start;
+
+	if (*core_count < 0) {
+		OFP_ERR("ERROR: At least 1 core is required.\n");
+		return -1;
+	}
+	if (*core_count > MAX_WORKERS)  {
+		OFP_ERR("ERROR: Number of processing cores %d"
+			" exeeds maximum number for this test %d.\n",
+			*core_count, MAX_WORKERS);
+		return -1;
+	}
 	return 0;
 }
 
@@ -301,7 +376,8 @@ static void parse_args(int argc, char *argv[], appl_args_t *appl_args)
 	size_t len;
 	int i;
 	static struct option longopts[] = {
-		{"count", required_argument, NULL, 'c'},
+		{"core_count", required_argument, NULL, 'c'},
+		{"core_start", required_argument, NULL, 's'},
 		{"interface", required_argument, NULL, 'i'},	/* return 'i' */
 		{"help", no_argument, NULL, 'h'},		/* return 'h' */
 		{"configuration file", required_argument,
@@ -310,9 +386,11 @@ static void parse_args(int argc, char *argv[], appl_args_t *appl_args)
 	};
 
 	memset(appl_args, 0, sizeof(*appl_args));
+	appl_args->core_start = 1;
+	appl_args->core_count = 0; /* all above core start */
 
 	while (1) {
-		opt = getopt_long(argc, argv, "+c:i:hf:",
+		opt = getopt_long(argc, argv, "+c:s:i:hf:",
 				  longopts, &long_index);
 
 		if (opt == -1)
@@ -321,6 +399,9 @@ static void parse_args(int argc, char *argv[], appl_args_t *appl_args)
 		switch (opt) {
 		case 'c':
 			appl_args->core_count = atoi(optarg);
+			break;
+		case 's':
+			appl_args->core_start = atoi(optarg);
 			break;
 			/* parse packet-io interface names */
 		case 'i':
@@ -446,7 +527,9 @@ static void usage(char *progname)
 		   "  -i, --interface Eth interfaces (comma-separated, no spaces)\n"
 		   "\n"
 		   "Optional OPTIONS\n"
-		   "  -c, --count <number> Core count.\n"
+		   "  -s, --core_start <number> Core start. Default 1.\n"
+		   "  -c, --core_count <number> Core count. Default 0: all above core start\n"
+		   "  -f, --configuration_file <file> OFP configuration file.\n"
 		   "  -h, --help           Display help and exit.\n"
 		   "\n", NO_PATH(progname), NO_PATH(progname)
 		);
