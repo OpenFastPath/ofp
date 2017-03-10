38192aefa6137877c3ae8b66df1877c987303130
lvsgate <lvsgate@163.com>
2017-03-10 15:25:08 +0800

    remove configure.sh

--- configure.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./configure --with-odp-lib=odp-dpdk --with-odp=/home/lijian/lib/odp --enable-shared=no --enable-sp=no --enable-debug --prefix=/home/lijian/lib/ofp --program-prefix=/home/lijian/lib/ofp
