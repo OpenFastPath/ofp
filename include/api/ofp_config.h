95d1a828c7577364c07f1ea036bbc3bfaf90274f
Jere Leppänen <jere.leppanen@nokia.com>
2017-04-05 12:07:41 +0300

    Enable UDP and TCP checksum validation.
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

--- include/api/ofp_config.h
+++ include/api/ofp_config.h
@@ -169,7 +169,7 @@
 /**Enable IPv4 UDP checksum validation mechanism on input
  * packets. If enabled, validation is performed on input
  * packets. */
-/*#define OFP_IPv4_UDP_CSUM_VALIDATE*/
+#define OFP_IPv4_UDP_CSUM_VALIDATE
 
 /**Enable IPv4 UDP checksum computation mechanism for outgoing
  * packets. If enabled, computation is performed based on
@@ -180,6 +180,6 @@
 /**Enable IPv4 TCP checksum validation mechanism on input
  * packets. If enabled, validation is performed on input
  * packets. */
-/*#define OFP_IPv4_TCP_CSUM_VALIDATE*/
+#define OFP_IPv4_TCP_CSUM_VALIDATE
 
 #endif
