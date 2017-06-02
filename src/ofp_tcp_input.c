OpenFastPath/ofp

commit af101aa4e648b020886fc36b51df4cc80e2c98a9

tcp: Fix data dropped when receive sockbuf is full.
Packets can be dropped if the receive sockbuf is full.
This happens after data has been acked.

Socket buffer space check was added before trying to add data to the sockbuf.

If there is no space the packet is dropped and ack the previous data to trigger
a retransmit.

Fixes: #93 Data corruption - tcp packets dropped in the receive path

Patch-by: Jason Preston <jpreston@infinite.io>
Signed-off-by: Sorin Vultureanu <Sorin.Vultureanu@enea.com>
Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

src/ofp_tcp_input.c
@@ -2788,49 +2788,58 @@ dodata:							/* XXX */
 		 * immediately when segments are out of order (so
 		 * fast retransmit can work).
 		 */
-		if (th->th_seq == tp->rcv_nxt &&
-		    OFP_LIST_EMPTY(&tp->t_segq) &&
-		    TCPS_HAVEESTABLISHED(tp->t_state)) {
-			if (DELAY_ACK(tp))
-				t_flags_or(tp->t_flags, TF_DELACK);
-			else
+		if (tlen <= sbspace(&so->so_rcv)) {
+			if (th->th_seq == tp->rcv_nxt &&
+			    OFP_LIST_EMPTY(&tp->t_segq) &&
+			    TCPS_HAVEESTABLISHED(tp->t_state)) {
+				if (DELAY_ACK(tp))
+					t_flags_or(tp->t_flags, TF_DELACK);
+				else
+					t_flags_or(tp->t_flags, TF_ACKNOW);
+				tp->rcv_nxt += tlen;
+				thflags = th->th_flags & OFP_TH_FIN;
+				TCPSTAT_INC(tcps_rcvpack);
+				TCPSTAT_ADD(tcps_rcvbyte, tlen);
+				ND6_HINT(tp);
+				SOCKBUF_LOCK(&so->so_rcv);
+				if (so->so_rcv.sb_state & SBS_CANTRCVMORE)
+					odp_packet_free(m);
+				else
+					ofp_sbappendstream_locked(&so->so_rcv,
+									m);
+				/* NB: sorwakeup_locked() does an implicit unlock. */
+				sorwakeup_locked(so);
+			} else {
+				/*
+				 * XXX: Due to the header drop above "th" is
+				 * theoretically invalid by now.  Fortunately
+				 * m_adj() doesn't actually frees any mbufs
+				 * when trimming from the head.
+				 */
+				thflags = ofp_tcp_reass(tp, th, &tlen, m);
 				t_flags_or(tp->t_flags, TF_ACKNOW);
-			tp->rcv_nxt += tlen;
-			thflags = th->th_flags & OFP_TH_FIN;
-			TCPSTAT_INC(tcps_rcvpack);
-			TCPSTAT_ADD(tcps_rcvbyte, tlen);
-			ND6_HINT(tp);
-			SOCKBUF_LOCK(&so->so_rcv);
-			if (so->so_rcv.sb_state & SBS_CANTRCVMORE)
-				odp_packet_free(m);
-			else
-				ofp_sbappendstream_locked(&so->so_rcv, m);
-			/* NB: sorwakeup_locked() does an implicit unlock. */
-			sorwakeup_locked(so);
-		} else {
+			}
+			if (tlen > 0 && (tp->t_flags & TF_SACK_PERMIT))
+				ofp_tcp_update_sack_list(tp, 
+							save_start,
+							save_start + tlen);
+#if 0
 			/*
-			 * XXX: Due to the header drop above "th" is
-			 * theoretically invalid by now.  Fortunately
-			 * m_adj() doesn't actually frees any mbufs
-			 * when trimming from the head.
+			 * Note the amount of data that peer has sent into
+			 * our window, in order to estimate the sender's
+			 * buffer size.
+			 * XXX: Unused.
 			 */
-			thflags = ofp_tcp_reass(tp, th, &tlen, m);
+			if (SEQ_GT(tp->rcv_adv, tp->rcv_nxt))
+				len = so->so_rcv.sb_hiwat -
+					(tp->rcv_adv - tp->rcv_nxt);
+			else
+				len = so->so_rcv.sb_hiwat;
+#endif
+		} else {
+			odp_packet_free(m);
 			t_flags_or(tp->t_flags, TF_ACKNOW);
 		}
-		if (tlen > 0 && (tp->t_flags & TF_SACK_PERMIT))
-			ofp_tcp_update_sack_list(tp, save_start, save_start + tlen);
-#if 0
-		/*
-		 * Note the amount of data that peer has sent into
-		 * our window, in order to estimate the sender's
-		 * buffer size.
-		 * XXX: Unused.
-		 */
-		if (SEQ_GT(tp->rcv_adv, tp->rcv_nxt))
-			len = so->so_rcv.sb_hiwat - (tp->rcv_adv - tp->rcv_nxt);
-		else
-			len = so->so_rcv.sb_hiwat;
-#endif
 	} else {
 		odp_packet_free(m);
 		thflags &= ~OFP_TH_FIN;

commit 9198abdc2c5e0c4929545e4a69f714e6549b8ea6

Fix premature closure of TCP connection
Fixes: #152 Premature closure of TCP connection

Clear the FIN flag for packets that are being dropped during so_rcv buffer
full condition.

This patch fixes a problem introduced by commit
af101aa4e648b020886fc36b51df4cc80e2c98a9 for bug #93 (Data corruption - tcp
packets dropped in the receive path).
If there is not enough room in the so_rcv sockbuf, the fix of bug #93
correctly drops the current packet to trigger a retransmit.
The problem occurs if that packet happens to be a FIN, in which case the
packet gets dropped but its FIN flag still gets processed, causing the
connection to close prematurely.
The patch below fixes the problem by clearing the FIN flag for such packets.

Signed-off-by: Janos Villanyi <a74esev@gmail.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_tcp_input.c
@@ -2849,6 +2849,7 @@ dodata:							/* XXX */
 		} else {
 			odp_packet_free(m);
 			t_flags_or(tp->t_flags, TF_ACKNOW);
+			thflags &= ~OFP_TH_FIN;
 		}
 	} else {
 		odp_packet_free(m);