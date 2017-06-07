OpenFastPath/ofp

commit 2073b2b1d5ba4a5d762513d473df7407e081e6aa

Fix OFP_DEBUG compile-time define
This patch fixes the situation where OFP_DEBUG_PRINT was always
passed at compile-time regardless of whether --enable-debug was
specified during the configuration stage.

With this patch, -DOFP_DEBUG will be passed iff --enable-debug
is specified. The OFP_DBG(..) logging macros will now be
compiled-out as intended.

Additionally, any code that is determined to be too much overhead
for a hot code path can be compiled out for a non-debug build:

  #if defined(OFP_DEBUG)
      // Additional debug code..
  #endif

Signed-off-by: Brian Brooks <brian.brooks@arm.com>
Reviewed-by: Jose Pekkarinen <jose.pekkarinen@nokia.com>

include/api/ofp_log.h
@@ -22,37 +22,84 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef OFP_DEBUG_PRINT
-#define OFP_DEBUG_PRINT 1
+/*
+ * These logging macros can be used to send a message to the logging
+ * destination. Currently, this is stderr.
+ *
+ * Log line format:
+ *
+ *   L t coreid:threadid file:line] msg...
+ *
+ * where the fields are defined as:
+ *
+ *   L        - A single character, representing the log level
+ *   t        - ODP tick count
+ *   coreid   - ODP core id
+ *   threadid - pthread thread id
+ *   file     - The file name
+ *   line     - The line number
+ *   msg      - The user-supplied message
+ *
+ * Example:
+ *
+ *   D 186 3:3098175232 ofp_pkt_processing.c:280] Device IP: 0.0.0.0, Packet Dest IP: 192.168.146.130
+ */
+#define OFP_INFO(fmt, ...) \
+	_OFP_LOG(OFP_LOG_INFO, fmt, ##__VA_ARGS__)
+#define OFP_WARN(fmt, ...) \
+	_OFP_LOG(OFP_LOG_WARNING, fmt, ##__VA_ARGS__)
+#define OFP_ERR(fmt, ...) \
+	_OFP_LOG(OFP_LOG_ERROR, fmt, ##__VA_ARGS__)
+#define OFP_ABORT(fmt, ...) \
+	_OFP_LOG(OFP_LOG_ABORT, fmt, ##__VA_ARGS__)
+
+/*
+ * Debug macros which will be compiled out when --enable-debug is not used.
+ */
+#if defined(OFP_DEBUG)
+#define OFP_DBG(fmt, ...) \
+	_OFP_LOG(OFP_LOG_DEBUG, fmt, ##__VA_ARGS__)
+#else
+#define OFP_DBG(fmt, ...) do {} while (0)
 #endif
 
-/**
- * log level.
- */
+#define OFP_LOG_NO_CTX(level, fmt, ...) do {		\
+		if (level > ofp_loglevel)		\
+			break;				\
+		fprintf(stderr, fmt, ##__VA_ARGS__);	\
+	} while (0)
+
 enum ofp_log_level_s {
 	OFP_LOG_ABORT = 1,
-	OFP_LOG_ERR,
+	OFP_LOG_ERROR,
+	OFP_LOG_WARNING,
 	OFP_LOG_INFO,
-	OFP_LOG_DBG
+	OFP_LOG_DEBUG,
+	OFP_LOG_MAX_LEVEL
 };
 
 extern enum ofp_log_level_s ofp_loglevel;
 
+static inline int ofp_debug_logging_enabled(void)
+{
+	return (ofp_loglevel == OFP_LOG_DEBUG);
+}
+
+/*
+ * Do not use these macros.
+ */
 #define __FILENAME__ \
 	(strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-
-/**
- * default LOG macro.
- */
-#define _ODP_FP_LOG(level, fmt, ...) do {				\
+#define _OFP_LOG(level, fmt, ...) do {					\
 		if (level > ofp_loglevel)				\
 			break;						\
 		fprintf(stderr, "%s %d %d:%u %s:%d] " fmt "\n",		\
-			(level == OFP_LOG_ABORT) ? "A" :		\
-			(level == OFP_LOG_ERR)   ? "E" :		\
-			(level == OFP_LOG_INFO)  ? "I" :		\
-			(level == OFP_LOG_DBG)   ? "D" : "?",		\
+			(level == OFP_LOG_ABORT)   ? "A" :		\
+			(level == OFP_LOG_ERROR)   ? "E" :		\
+			(level == OFP_LOG_WARNING) ? "W" :		\
+			(level == OFP_LOG_INFO)    ? "I" :		\
+			(level == OFP_LOG_DEBUG)   ? "D" : "?",		\
 			ofp_timer_ticks(0),				\
 			odp_cpu_id(), (unsigned int) pthread_self(),	\
 			__FILENAME__, __LINE__,				\
@@ -60,62 +107,5 @@ extern enum ofp_log_level_s ofp_loglevel;
 		if (level == OFP_LOG_ABORT)				\
 			abort();					\
 	} while (0)
-
-/**
- * Debug printing macro, which prints output when DEBUG flag is set.
- */
-#if (OFP_DEBUG_PRINT == 1)
-# define OFP_DBG(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_DBG, fmt, ##__VA_ARGS__)
-# define OFP_IS_LOGLEVEL_DEBUG() \
-		(ofp_loglevel == OFP_LOG_DBG ? 1 : 0)
-#else
-# define OFP_DBG(fmt, ...)
-# define OFP_IS_LOGLEVEL_DEBUG() 0
-#endif
-
-/**
- * Print output to stderr (file, line and function).
- */
-#define OFP_ERR(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_ERR, fmt, ##__VA_ARGS__)
-
-/**
- * Print output to stderr (file, line and function),
- * then abort.
- */
-#define OFP_ABORT(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_ABORT, fmt, ##__VA_ARGS__)
-
-/**
- * Print output to stderr (file, line and function)
- */
-#define OFP_LOG(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_INFO, fmt, ##__VA_ARGS__)
-
-/**
- * Print output to stderr (file, line and function)
- */
-#define OFP_INFO(fmt, ...) \
-		_ODP_FP_LOG(OFP_LOG_INFO, fmt, ##__VA_ARGS__)
-
-/**
- * Print output to stderr
- */
-#define OFP_LOG_NO_CTX(level, fmt, ...) \
-do { \
-	if (level > ofp_loglevel) \
-		break; \
-	fprintf(stderr, fmt, ##__VA_ARGS__); \
-} while (0)
-
-/**
- * Intentionally unused variables to functions
- */
-#define OFP_UNUSED     __attribute__((__unused__))
-
-/**
- * @}
- */
 
 #endif /*__OFP_LOG_H__*/

src/cli/ofp_cli_log.c
@@ -53,7 +53,7 @@ void f_loglevel(struct cli_conn *conn, const char *s)
 {
 	int i;
 
-	for (i = 0; i <= OFP_LOG_DBG; i++) {
+	for (i = 0; i < OFP_LOG_MAX_LEVEL; i++) {
 		if (strncmp(loglevel_descript[i], s,
 			strlen(loglevel_descript[i])) == 0) {
 			ofp_loglevel = i;

src/ofp_log.c
@@ -7,4 +7,4 @@
 
 #include "ofpi_log.h"
 
-enum ofp_log_level_s ofp_loglevel = OFP_LOG_DBG;
+enum ofp_log_level_s ofp_loglevel = OFP_LOG_DEBUG;

configure.ac
@@ -134,14 +134,14 @@ else
 		])
 fi
 
-# Enable/disable OFP_DEBUG_PRINT
-ODP_DEBUG=1
+# Enable/disable OFP_DEBUG
+ofp_debug=no
 AC_ARG_ENABLE([debug],
-    [  --enable-debug         Enable/disable ODP debug],
-    [if ! test "x$enableval" = "xyes"; then
-        ODP_DEBUG=0
-    fi])
-AM_CFLAGS="$AM_CFLAGS -DOFP_DEBUG_PRINT=$ODP_DEBUG"
+    [  --enable-debug         Enable OFP debug],
+    [ofp_debug=$enableval])
+if test "$ofp_debug" == "yes" ; then
+    AM_CFLAGS="$AM_CFLAGS -DOFP_DEBUG"
+fi
 
 # Enable/disable INET6 domain
 AC_ARG_ENABLE([ipv6],

example/ioctl_test/ioctl_test.c
@@ -17,8 +17,7 @@
 
 #define logfilename "/tmp/iocrl-test.log"
 static FILE *logfile;
-#define logprint(a...) fprintf(logfile, a)
-//#define logprint OFP_LOG
+
 #define IFNAME "fp0"
 #define GRENAME "gre1"
 
@@ -31,8 +30,8 @@ get_ip_address(int fd, const char *name)
 
 	strcpy(ifr.ifr_name, name);
 	if (ofp_ioctl(fd, OFP_SIOCGIFADDR, &ifr) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 		return 0;
 	} else {
 		struct ofp_sockaddr_in *ipaddr;
@@ -48,8 +47,8 @@ get_netmask(int fd, const char *name)
 
 	strcpy(ifr.ifr_name, name);
 	if (ofp_ioctl(fd, OFP_SIOCGIFNETMASK, &ifr) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 		return 0;
 	} else {
 		struct ofp_sockaddr_in *ipaddr;
@@ -65,8 +64,8 @@ get_broadcast_address(int fd, const char *name)
 
 	strcpy(ifr.ifr_name, name);
 	if (ofp_ioctl(fd, OFP_SIOCGIFBRDADDR, &ifr) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 		return 0;
 	} else {
 		struct ofp_sockaddr_in *ipaddr;
@@ -85,8 +84,8 @@ set_ip_address_and_mask(int fd, const char *name, uint32_t addr, uint32_t mask)
 	ifra.ifra_addr.sin_addr.s_addr = addr;
 	ifra.ifra_mask.sin_addr.s_addr = mask;
 	if (ofp_ioctl(fd, OFP_SIOCSIFADDR, &ifra) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 }
 
@@ -98,8 +97,8 @@ delete_if_address(int fd, const char *name)
 	strcpy(ifra.ifra_name, name);
 	ifra.ifra_addr.sin_family = OFP_AF_INET;
 	if (ofp_ioctl(fd, OFP_SIOCDIFADDR, &ifra) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 }
 
@@ -121,15 +120,15 @@ receive_non_blocking(void)
 	addr.sin_len = sizeof(addr);
 
 	if ((ret = ofp_bind(s, (struct ofp_sockaddr *)&addr, sizeof(addr)))) {
-		OFP_LOG("bind ret=%d %s\n", ret, ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_bind failed ret=%d %s", ret, ofp_strerror(ofp_errno));
 	}
 
 	/*
 	 * Set non-blocking mode.
 	 */
 	if (ofp_ioctl(s, OFP_FIONBIO, &nb) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 
 	/*
@@ -145,8 +144,8 @@ get_sockbuf_data(int fd, uint32_t cmd)
 {
 	int val;
 	if (ofp_ioctl(fd, cmd, &val) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 	return val;
 }
@@ -154,12 +153,9 @@ get_sockbuf_data(int fd, uint32_t cmd)
 static void
 get_if_conf(int fd, struct ofp_ifconf *conf)
 {
-	/*
-	 * Get all interfaces.
-	 */
 	if (ofp_ioctl(fd, OFP_SIOCGIFCONF, conf) < 0) {
-		logprint("Ioctlx error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 }
 
@@ -177,8 +173,8 @@ set_gre_tunnel(int fd, const char *name, uint32_t addr, uint32_t p2p,
 	treq.iftun_vrf = vrf;
 
 	if (ofp_ioctl(fd, OFP_SIOCSIFTUN, &treq) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 }
 
@@ -191,8 +187,8 @@ set_vrf(int fd, const char *name, int vrf)
 	ifr.ifr_fib = vrf;
 
 	if (ofp_ioctl(fd, OFP_SIOCSIFFIB, &ifr) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 }
 
@@ -209,8 +205,8 @@ set_route(int fd, const char *dev, int vrf,
 	((struct ofp_sockaddr_in *)&rt.rt_gateway)->sin_addr.s_addr = gw;
 
 	if (ofp_ioctl(fd, OFP_SIOCADDRT, &rt) < 0) {
-		logprint("Ioctl error (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_ioctl failed, err='%s'",
+			ofp_strerror(ofp_errno));
 	}
 }
 
@@ -223,76 +219,76 @@ ioctl_test(void *arg)
 	(void)arg;
 
 	logfile = fopen(logfilename, "w");
-	logprint("Ioctl test thread started\n");
+	OFP_INFO("ioctl_test thread started");
 
 	odp_init_local(ODP_THREAD_CONTROL);
 	ofp_init_local();
 	sleep(2);
 
 	if ((fd = ofp_socket(OFP_AF_INET, OFP_SOCK_DGRAM, OFP_IPPROTO_UDP)) < 0) {
-		logprint("Cannot open UDP socket (%s)!\n",
-			 ofp_strerror(ofp_errno));
+		OFP_ERR("ofp_socket failed, err='%s'",
+			ofp_strerror(ofp_errno));
 		return NULL;
 	}
 
-	logprint("\n=====================================\n");
-	logprint("Get IP address of %s\n", IFNAME);
+	OFP_INFO("=====================================");
+	OFP_INFO("Get IP address of %s", IFNAME);
 	origaddr = get_ip_address(fd, IFNAME);
-	logprint("  %s\n", ofp_print_ip_addr(origaddr));
+	OFP_INFO("  %s", ofp_print_ip_addr(origaddr));
 
-	logprint("\n=====================================\n");
-	logprint("Get netmask of %s\n", IFNAME);
+	OFP_INFO("=====================================");
+	OFP_INFO("Get netmask of %s", IFNAME);
 	origmask = get_netmask(fd, IFNAME);
-	logprint("  %s\n", ofp_print_ip_addr(origmask));
+	OFP_INFO("  %s", ofp_print_ip_addr(origmask));
 
-	logprint("\n=====================================\n");
-	logprint("Get broadcast address of %s\n", IFNAME);
-	logprint("  %s\n",
+	OFP_INFO("=====================================");
+	OFP_INFO("Get broadcast address of %s", IFNAME);
+	OFP_INFO("  %s",
 		 ofp_print_ip_addr(get_broadcast_address(fd, IFNAME)));
 
-	logprint("\n=====================================\n");
-	logprint("Delete IP address of %s\n", IFNAME);
+	OFP_INFO("=====================================");
+	OFP_INFO("Delete IP address of %s", IFNAME);
 	delete_if_address(fd, IFNAME);
 
-	logprint("\n=====================================\n");
+	OFP_INFO("=====================================");
 	addr = IP4(192,168,156,111);
-	logprint("Set IP address of %s to %s/%d\n",
+	OFP_INFO("Set IP address of %s to %s/%d",
 		 IFNAME, ofp_print_ip_addr(addr), 25);
 	set_ip_address_and_mask(fd, IFNAME, addr, odp_cpu_to_be_32(0xffffff80));
 
-	logprint("Set back original address and mask\n");
+	OFP_INFO("Set back original address and mask");
 	set_ip_address_and_mask(fd, IFNAME, origaddr, origmask);
 
-	logprint("\n=====================================\n");
-	logprint("Receiving from socket\n");
+	OFP_INFO("=====================================");
+	OFP_INFO("Receiving from socket");
 	receive_non_blocking();
-	logprint("Immediate return\n");
+	OFP_INFO("Immediate return");
 
-	logprint("\n=====================================\n");
-	logprint("Get sockbuf bytes to read\n");
-	logprint("  %d\n", get_sockbuf_data(fd, OFP_FIONREAD));
+	OFP_INFO("=====================================");
+	OFP_INFO("Get sockbuf bytes to read");
+	OFP_INFO("  %d", get_sockbuf_data(fd, OFP_FIONREAD));
 
-	logprint("\n=====================================\n");
-	logprint("Get sockbuf bytes yet to write\n");
+	OFP_INFO("=====================================");
+	OFP_INFO("Get sockbuf bytes yet to write");
 
-	logprint("  %d\n", get_sockbuf_data(fd, OFP_FIONWRITE));
+	OFP_INFO("  %d", get_sockbuf_data(fd, OFP_FIONWRITE));
 
-	logprint("\n=====================================\n");
-	logprint("Get sockbuf send space\n");
+	OFP_INFO("=====================================");
+	OFP_INFO("Get sockbuf send space");
 
-	logprint("  %d\n", get_sockbuf_data(fd, OFP_FIONSPACE));
+	OFP_INFO("  %d", get_sockbuf_data(fd, OFP_FIONSPACE));
 
-	logprint("\n=====================================\n");
-	logprint("Set GRE tunnel\n");
+	OFP_INFO("=====================================");
+	OFP_INFO("Set GRE tunnel");
 	set_gre_tunnel(fd, GRENAME, IP4(10,3,4,1), IP4(10,3,4,2),
 		       origaddr, IP4(192,168,56,104), 0);
 
-	logprint("\n=====================================\n");
-	logprint("Change GRE tunnel's VRF\n");
+	OFP_INFO("=====================================");
+	OFP_INFO("Change GRE tunnel's VRF");
 	set_vrf(fd, GRENAME, 7);
 
-	logprint("\n=====================================\n");
-	logprint("Get all interfaces\n");
+	OFP_INFO("=====================================");
+	OFP_INFO("Get all interfaces");
 
 	struct ofp_ifconf conf;
 	char data[1024];
@@ -308,7 +304,7 @@ ioctl_test(void *arg)
 	while ((char *)ifr < data + conf.ifc_len) {
 		switch (ifr->ifr_addr.sa_family) {
 		case OFP_AF_INET:
-			logprint("  %d. %s : %s\n", i, ifr->ifr_name,
+			OFP_INFO("  %d. %s : %s", i, ifr->ifr_name,
 				 ofp_print_ip_addr(((struct ofp_sockaddr_in *)
 						      &ifr->ifr_addr)->sin_addr.s_addr));
 			break;
@@ -317,8 +313,8 @@ ioctl_test(void *arg)
 		i++;
 	}
 
-	logprint("\n=====================================\n");
-	logprint("Set routes\n");
+	OFP_INFO("=====================================");
+	OFP_INFO("Set routes");
 
 	set_route(fd, GRENAME, 0, IP4(10,1,1,0), IP4(255,255,255,0), IP4(10,3,4,2));
 	/*
@@ -326,14 +322,14 @@ ioctl_test(void *arg)
 	 */
 	set_route(fd, NULL, 0, IP4(10,7,0,0), IP4(255,255,0,0), IP4(192,168,56,254));
 
-	logprint("\n=====================================\n");
+	OFP_INFO("=====================================");
 	ofp_close(fd);
-	logprint("Ioctl test exit\n");
-	logprint("\n=====================================\n");
+	OFP_INFO("Ioctl test exit");
+	OFP_INFO("=====================================");
 
 	fclose(logfile);
 	if (system("cat " logfilename) < 0)
-		OFP_ERR("Cannot run system()\n");
+		OFP_ERR("system failed");
 	return NULL;
 }
 

example/sysctl/sysctl.c
@@ -207,14 +207,14 @@ sysctl(void *arg)
 	size_t retval;
 	ofp_sysctl("mybranch.telnet.counter", &counter, &counterlen,
 		     NULL, 0, &retval);
-	OFP_LOG("mybranch.telnet.counter=%ld len=%ld retval=%ld\n",
+	OFP_INFO("mybranch.telnet.counter=%ld len=%ld retval=%ld\n",
 		  counter, counterlen, retval);
 	/*
 	 * Read the ssh bytes:
 	 */
 	ofp_sysctl("mybranch.ssh.counter", &counter, &counterlen,
 		     NULL, 0, &retval);
-	OFP_LOG("mybranch.ssh.counter=%ld len=%ld retval=%ld\n",
+	OFP_INFO("mybranch.ssh.counter=%ld len=%ld retval=%ld\n",
 		  counter, counterlen, retval);
 
 	/*
@@ -224,7 +224,7 @@ sysctl(void *arg)
 	size_t enalen = sizeof(enabled);
 	ofp_sysctl("mybranch.telnet.enabled", &enabled, &enalen,
 		     NULL, 0, &retval);
-	OFP_LOG("mybranch.telnet.enabled=%d\n", enabled);
+	OFP_INFO("mybranch.telnet.enabled=%d\n", enabled);
 	/*
 	 * Disable telnet:
 	 */
@@ -239,7 +239,7 @@ sysctl(void *arg)
 	enalen = sizeof(enabled);
 	ofp_sysctl("mybranch.telnet.enabled", &enabled, &enalen,
 		     NULL, 0, &retval);
-	OFP_LOG("After disabling: mybranch.telnet.enabled=%d, real value=%d\n",
+	OFP_INFO("After disabling: mybranch.telnet.enabled=%d, real value=%d\n",
 		  enabled, enable_telnet);
 
 	/*
@@ -249,7 +249,7 @@ sysctl(void *arg)
 	size_t msglen = sizeof(msg);
 	ofp_sysctl("mybranch.hello", msg, &msglen,
 		     "Server is down.", 16, &retval);
-	OFP_LOG("mybranch.hello: old value=%s, new value=%s\n",
+	OFP_INFO("mybranch.hello: old value=%s, new value=%s\n",
 		  msg, hello_msg);
 
 	/*

example/udpecho/udp_server.c
@@ -19,9 +19,6 @@
 #define INVALID_SOCKET  -1
 #define SOCKET_ERROR    -1
 
-//#define logprint(a...) do {} while (0)
-#define logprint OFP_LOG
-
 static void notify(union ofp_sigval sv)
 {
 	struct ofp_sock_sigval *ss = sv.sival_ptr;
@@ -89,7 +86,7 @@ static void *udpecho(void *arg)
 
 	(void)arg;
 
-	logprint("UDP server thread started\n");
+	OFP_INFO("UDP server thread started");
 
 	odp_init_local(ODP_THREAD_CONTROL);
 	ofp_init_local();
@@ -98,7 +95,7 @@ static void *udpecho(void *arg)
 	my_ip_addr = ofp_port_get_ipv4_addr(0, 0, OFP_PORTCONF_IP_TYPE_IP_ADDR);
 
 	if ((serv_fd = ofp_socket(OFP_AF_INET, OFP_SOCK_DGRAM, OFP_IPPROTO_UDP)) < 0) {
-		logprint("Cannot open UDP socket (%s)!\n",
+		OFP_ERR("ofp_socket failed, err='%s'",
 			 ofp_strerror(ofp_errno));
 		return NULL;
 	}
@@ -111,7 +108,7 @@ static void *udpecho(void *arg)
 
 	if (ofp_bind(serv_fd, (struct ofp_sockaddr *)&my_addr,
 		       sizeof(struct ofp_sockaddr)) < 0) {
-		logprint("Cannot bind http socket (%s)!\n",
+		OFP_ERR("ofp_bind failed, err='%s'",
 			 ofp_strerror(ofp_errno));
 		return NULL;
 	}
@@ -133,7 +130,7 @@ static void *udpecho(void *arg)
 		sleep(1);
 	}
 
-	logprint("UDP server exit\n");
+	OFP_INFO("UDP server exiting");
 	return NULL;
 }
 

example/webserver/httpd.c
@@ -11,9 +11,6 @@
 
 void httpd_main(uint32_t addr);
 
-#define logprint(a...) do {} while (0)
-//#define logprint OFP_LOG
-
 int sigreceived = 0;
 static uint32_t myaddr;
 
@@ -37,14 +34,14 @@ static int mysend(int s, char *p, int len)
 	while (len > 0) {
 		n = ofp_send(s, p, len, 0);
 		if (n < 0) {
-			OFP_LOG("mysend: cannot send (%d): %s\n",
+			OFP_ERR("ofp_send failed n=%d, err='%s'",
 				  n, ofp_strerror(ofp_errno));
 			return n;
 		}
 		len -= n;
 		p += n;
 		if (len) {
-			logprint("mysend: only %d bytes sent\n", n);
+			OFP_WARN("Only %d bytes sent", n);
 		}
 	}
 	return len;
@@ -123,11 +120,11 @@ static int analyze_http(char *http, int s) {
 			*p = 0;
 		else
 			return -1;
-		logprint("GET %s (fd=%d)\n", url, s);
+		OFP_INFO("GET %s (fd=%d)", url, s);
 		get_file(s, url);
 	} else if (!strncmp(http, "POST ", 5)) {
 		/* Post is not supported. */
-		logprint("%s\n", http);
+		OFP_INFO("%s", http);
 	}
 
 	return 0;
@@ -142,7 +139,7 @@ static void *webserver(void *arg)
 
 	(void)arg;
 
-	logprint("HTTP thread started\n");
+	OFP_INFO("HTTP thread started");
 
 	odp_init_local(ODP_THREAD_CONTROL);
 	ofp_init_local();
@@ -151,8 +148,8 @@ static void *webserver(void *arg)
 	myaddr = ofp_port_get_ipv4_addr(0, 0, OFP_PORTCONF_IP_TYPE_IP_ADDR);
 
 	if ((serv_fd = ofp_socket(OFP_AF_INET, OFP_SOCK_STREAM, OFP_IPPROTO_TCP)) < 0) {
+		OFP_ERR("ofp_socket failed");
 		perror("serv socket");
-		logprint("Cannot open http socket!\n");
 		return NULL;
 	}
 
@@ -164,7 +161,7 @@ static void *webserver(void *arg)
 
 	if (ofp_bind(serv_fd, (struct ofp_sockaddr *)&my_addr,
 		       sizeof(struct ofp_sockaddr)) < 0) {
-		logprint("Cannot bind http socket (%s)!\n", ofp_strerror(ofp_errno));
+		OFP_ERR("Cannot bind http socket (%s)!", ofp_strerror(ofp_errno));
 		return 0;
 	}
 
@@ -190,14 +187,14 @@ static void *webserver(void *arg)
 			if ((tmp_fd = ofp_accept(serv_fd,
 						   (struct ofp_sockaddr *)&caller,
 						   &alen)) > 0) {
-				logprint("accept %d\n", tmp_fd);
+				OFP_INFO("accept fd=%d", tmp_fd);
 
 				for (i = 0; i < NUM_CONNECTIONS; i++)
 					if (connections[i].fd == 0)
 						break;
 
 				if (i >= NUM_CONNECTIONS) {
-					logprint("Node cannot accept new connections!\n");
+					OFP_ERR("Node cannot accept new connections!");
 					ofp_close(tmp_fd);
 					continue;
 				}
@@ -211,7 +208,7 @@ static void *webserver(void *arg)
 							  OFP_SO_LINGER,
 							  &so_linger,
 							  sizeof so_linger);
-				if (r1) OFP_LOG("SO_LINGER failed!\n");
+				if (r1) OFP_ERR("SO_LINGER failed!");
 #endif
 				struct ofp_timeval tv;
 				tv.tv_sec = 3;
@@ -221,7 +218,7 @@ static void *webserver(void *arg)
 							  OFP_SO_SNDTIMEO,
 							  &tv,
 							  sizeof tv);
-				if (r2) OFP_LOG("SO_SNDTIMEO failed!\n");
+				if (r2) OFP_ERR("SO_SNDTIMEO failed!");
 
 				connections[i].fd = tmp_fd;
 				connections[i].addr = caller.sin_addr.s_addr;
@@ -241,26 +238,26 @@ static void *webserver(void *arg)
 			r = ofp_recv(connections[i].fd, buf, sizeof(buf)-1, 0);
 			if (r > 0) {
 				buf[r] = 0;
-				logprint("DATA='%s'\n", buf);
+				OFP_INFO("recv data: %s", buf);
 
 				if (!strncmp(buf, "GET", 3))
 					analyze_http(buf, connections[i].fd);
 				else
-					logprint("http req error\n");
+					OFP_INFO("Not a HTTP GET request");
 
-				logprint("closing %d\n", connections[i].fd);
+				OFP_INFO("closing %d\n", connections[i].fd);
 				OFP_FD_CLR(connections[i].fd, &read_fd);
 				while (ofp_close(connections[i].fd) < 0) {
-					OFP_LOG("Socket %d close err: %s\n",
-						  connections[i].fd,
-						  ofp_strerror(ofp_errno));
+					OFP_ERR("ofp_close failed, fd=%d err='%s'",
+						connections[i].fd,
+						ofp_strerror(ofp_errno));
 					sleep(1);
 				}
-				logprint("closed %d\n", connections[i].fd);
+				OFP_INFO("closed fd=%d", connections[i].fd);
 				connections[i].fd = 0;
 			} else if (r == 0) {
 				if (connections[i].post) {
-					printf("file download finished\n");
+					OFP_INFO("File download finished");
 					fclose(connections[i].post);
 					connections[i].post = NULL;
 				}
@@ -271,7 +268,7 @@ static void *webserver(void *arg)
 		}
 	}
 
-	logprint("httpd exit\n");
+	OFP_INFO("httpd exiting");
 	return NULL;
 }
 

commit 2ae11ac416a296e3cc65d89e67a0b621a4bb9fa9

Fix description and logic of "loglevel" CLI command
Corrected mismatch between loglevel_descript and ofp_loglevel
values. Set ofp_loglevel initial value according to OFP_DEBUG
macro.

Fixes: #60 "loglevel" is broken after commit 2073b2b1
Fixes: #61 "Loglevel" description is missing

Signed-off-by: Viktor Tikkanen <viktor.tikkanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

example/socket/ofp.conf
@@ -1,4 +1,4 @@
 debug 0
-loglevel set abort
+loglevel set info
 ifconfig fp0 192.168.100.1/24
 ifconfig -A inet6 fp0 fd00:1baf::1/64

example/webserver2/ofp.conf
@@ -1,3 +1,3 @@
 debug 0
-loglevel set abort
+loglevel set info
 ifconfig fp0 192.168.100.1/24

include/api/ofp_log.h
@@ -75,8 +75,8 @@
 		fprintf(stderr, fmt, ##__VA_ARGS__)
 
 enum ofp_log_level_s {
-	OFP_LOG_DISABLED = -1,
-	OFP_LOG_ERROR = 0,
+	OFP_LOG_DISABLED = 0,
+	OFP_LOG_ERROR,
 	OFP_LOG_WARNING,
 	OFP_LOG_INFO,
 	OFP_LOG_DEBUG,

src/cli/ofp_cli_log.c
@@ -14,10 +14,11 @@
 #include "ofpi_util.h"
 
 const char *loglevel_descript[] = {
-	"abort",
-	"error",
-	"info",
-	"debug"
+        [OFP_LOG_DISABLED] = "disabled",
+        [OFP_LOG_ERROR]    = "error",
+        [OFP_LOG_WARNING]  = "warning",
+        [OFP_LOG_INFO]     = "info",
+        [OFP_LOG_DEBUG]    = "debug"
 };
 
 /* loglevel help */
@@ -27,10 +28,13 @@ void f_help_loglevel(struct cli_conn *conn, const char *s)
 	(void)s;
 
 	ofp_sendf(conn->fd, "Show log level\r\n"
-		"  loglevel show\r\n");
+		"  loglevel show\r\n\r\n");
 	ofp_sendf(conn->fd, "Set log level\r\n"
-		"  loglevel set <debug|info|error|abort>\r\n"
-		"  Example: loglevel set debug\r\n");
+		"  loglevel set <debug|info|warning|error|disabled>\r\n"
+                "  Note:\r\n"
+                "    Debug level logs require --enable-debug configuration option\r\n"
+                "  Example: Set log level to generate warning and error logs:\r\n"
+		"    loglevel set warning\r\n\r\n");
 	ofp_sendf(conn->fd, "Show log level help (this help)\r\n"
 		"  loglevel help\r\n");
 
@@ -48,7 +52,7 @@ void f_loglevel_show(struct cli_conn *conn, const char *s)
 	sendcrlf(conn);
 }
 
-/* loglevel <debug|info|error|abort> */
+/* loglevel set */
 void f_loglevel(struct cli_conn *conn, const char *s)
 {
 	int i;

src/ofp_igmp.c
@@ -3586,8 +3586,6 @@ igmp_rec_type_to_str(const int type)
 void
 ofp_igmp_init(void)
 {
-	ofp_loglevel = 10;
-
 	CTR1(KTR_IGMPV3, "%s: initializing", __func__);
 
 	IGMP_LOCK_INIT();

src/ofp_log.c
@@ -7,4 +7,8 @@
 
 #include "ofpi_log.h"
 
+#ifdef OFP_DEBUG
 enum ofp_log_level_s ofp_loglevel = OFP_LOG_DEBUG;
+#else
+enum ofp_log_level_s ofp_loglevel = OFP_LOG_INFO;
+#endif