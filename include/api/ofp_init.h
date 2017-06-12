7cbca9ceb16aee30257013ae510eb241e5cd6e3d
Jere Leppänen <jere.leppanen@nokia.com>
2017-06-12 15:13:05 +0300

    Tweak ofp_global_param_t documentation.
    - Name struct arp_s. At this point, no effect on anything or anyone
      apart from doxygen.
    
    - Fix comment style.
    
    - A couple of other very minor comment changes.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

--- include/api/ofp_init.h
+++ include/api/ofp_init.h
@@ -35,40 +35,62 @@
  * @see ofp_init_global_param()
  */
 typedef struct ofp_global_param_t {
-	/** Count of interfaces to be initialized. The default value is 0. */
+	/**
+	 * Count of interfaces to be initialized. The default value is
+	 * 0.
+	 */
 	uint16_t if_count;
 
-	/** CPU core to which internal OFP control threads are pinned.
-	 *  The default value is 0. */
+	/**
+	 * CPU core to which internal OFP control threads are pinned.
+	 * The default value is 0.
+	 */
 	uint16_t linux_core_id;
 
-	/** Names of the interfaces to be initialized. The naming convention
-	 *  depends on the operating system and the ODP implementation.
-	 *  Must point to an array of at least if_count zero terminated
-	 *  strings. */
+	/**
+	 * Names of the interfaces to be initialized. The naming
+	 * convention depends on the operating system and the ODP
+	 * implementation. Must point to an array of at least if_count
+	 * zero terminated strings.
+	 */
 	char **if_names;
 
-	/** ODP event scheduling group for all scheduled event queues
-	 *  (pktio queues, timer queues and other queues) created in OFP
-	 *  initialization. The default value is ODP_SCHED_GROUP_ALL. */
+	/**
+	 * ODP event scheduling group for all scheduled event queues
+	 * (pktio queues, timer queues and other queues) created in
+	 * OFP initialization. The default value is
+	 * ODP_SCHED_GROUP_ALL.
+	 */
 	odp_schedule_group_t sched_group;
 
-	/** Packet processing hooks. @see ofp_hook.h
-	 *  The default value is NULL for every hook. */
+	/**
+	 * Packet processing hooks. The default value is NULL for
+	 * every hook.
+	 *
+	 * @see ofp_hook.h
+	 */
 	ofp_pkt_hook pkt_hook[OFP_HOOK_MAX];
 
-	/** Use direct input mode for all interfaces if set. Otherwise use
-	 *  scheduled input mode. Default value is 0 (i.e. scheduled mode). */
+	/**
+	 * Use direct input mode for all interfaces if set. Otherwise
+	 * use scheduled input mode. Default value is 0
+	 * (i.e. scheduled mode).
+	 */
 	uint8_t burst_recv_mode;
 
-	/** Create netlink listener thread. If slow path is enabled,
-	 *  then default is TRUE, otherwise default is FALSE. */
+	/**
+	 * Create netlink listener thread. If slow path is enabled,
+	 * then default is TRUE, otherwise default is FALSE.
+	 */
 	odp_bool_t enable_nl_thread;
 
-	struct {
-		/// ARP aging timer interval in seconds. Default is ARP_AGE_INTERVAL.
+	/**
+	 * Global ARP parameters.
+	 */
+	struct arp_s {
+		/** Aging timer interval in seconds. Default is ARP_AGE_INTERVAL. */
 		int age_interval;
-		/// ARP entry timeout in seconds. Default is ARP_ENTRY_TIMEOUT.
+		/** Entry timeout in seconds. Default is ARP_ENTRY_TIMEOUT. */
 		int entry_timeout;
 	} arp;
 } ofp_global_param_t;
