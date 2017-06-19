OpenFastPath/ofp

commit f65efd2cd9a984d21a9416e2a452f16fa132638f

Add new init parameter enable_nl_thread.
Create a netlink listener thread only if slow path is enabled and
enable_nl_thread is nonzero. This is the default.

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_init.c
@@ -104,6 +104,9 @@ void ofp_init_global_param(ofp_init_global_t *params)
 {
 	memset(params, 0, sizeof(*params));
 	params->sched_group = ODP_SCHED_GROUP_ALL;
+#ifdef SP
+	params->enable_nl_thread = 1;
+#endif /* SP */
 }
 
 int ofp_init_pre_global(const char *pool_name_unused,
@@ -185,9 +188,7 @@ int ofp_init_global(odp_instance_t instance, ofp_init_global_t *params)
 	int i;
 	odp_pktio_param_t pktio_param;
 	odp_pktin_queue_param_t pktin_param;
-#ifdef SP
 	odph_linux_thr_params_t thr_params;
-#endif /* SP */
 
 	ofp_init_global_called = 1;
 
@@ -217,21 +218,18 @@ int ofp_init_global(odp_instance_t instance, ofp_init_global_t *params)
 		HANDLE_ERROR(ofp_ifnet_create(instance, params->if_names[i],
 			&pktio_param, &pktin_param, NULL));
 
-#ifdef SP
-	/* Start Netlink server process */
-	thr_params.start = START_NL_SERVER;
-	thr_params.arg = NULL;
-	thr_params.thr_type = ODP_THREAD_CONTROL;
-	thr_params.instance = instance;
-	if (!odph_linux_pthread_create(&shm->nl_thread,
-				  &cpumask,
-				  &thr_params)) {
-
-		OFP_ERR("Failed to start Netlink thread.");
-		return -1;
+	if (params->enable_nl_thread) {
+		/* Start Netlink server process */
+		thr_params.start = START_NL_SERVER;
+		thr_params.arg = NULL;
+		thr_params.thr_type = ODP_THREAD_CONTROL;
+		thr_params.instance = instance;
+		if (!odph_linux_pthread_create(&shm->nl_thread, &cpumask, &thr_params)) {
+			OFP_ERR("Failed to start Netlink thread.");
+			return -1;
+		}
+		shm->nl_thread_is_running = 1;
 	}
-	shm->nl_thread_is_running = 1;
-#endif /* SP */
 
 	odp_schedule_resume();
 	return 0;

include/api/ofp_init.h
@@ -60,6 +60,10 @@ typedef struct ofp_init_global_t {
 	/** Use direct input mode for all interfaces if set. Otherwise use
 	 *  scheduled input mode. Default value is 0 (i.e. scheduled mode). */
 	uint8_t burst_recv_mode;
+
+	/** Create netlink listener thread. If slow path is enabled,
+	 *  then default is TRUE, otherwise default is FALSE. */
+	odp_bool_t enable_nl_thread;
 } ofp_init_global_t;
 
 /**

commit e16d97dba9c43968fa887cb77541fcb3ccbce83f

Fix compilation with --enable-sp=no.
Put back an #ifdef SP directive that was removed in an earlier
commit. It's needed after all when compiling without slow path
support.

Fixes: f65efd2 Add new init parameter enable_nl_thread.

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_init.c
@@ -190,7 +190,6 @@ int ofp_init_global(odp_instance_t instance, ofp_global_param_t *params)
 	int i;
 	odp_pktio_param_t pktio_param;
 	odp_pktin_queue_param_t pktin_param;
-	odph_linux_thr_params_t thr_params;
 
 	ofp_init_global_called = 1;
 
@@ -217,7 +216,10 @@ int ofp_init_global(odp_instance_t instance, ofp_global_param_t *params)
 		HANDLE_ERROR(ofp_ifnet_create(instance, params->if_names[i],
 			&pktio_param, &pktin_param, NULL));
 
+#ifdef SP
 	if (params->enable_nl_thread) {
+		odph_linux_thr_params_t thr_params;
+
 		/* Start Netlink server process */
 		thr_params.start = START_NL_SERVER;
 		thr_params.arg = NULL;
@@ -229,6 +231,7 @@ int ofp_init_global(odp_instance_t instance, ofp_global_param_t *params)
 		}
 		shm->nl_thread_is_running = 1;
 	}
+#endif /* SP */
 
 	odp_schedule_resume();
 	return 0;