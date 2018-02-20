OpenFastPath/ofp

commit 51312a31aa4d176c6bec2cf0310e1a01d3a880c8

Fix the packet handle lost issue in case of reassembly
Signed-off-by: Iulia Florea <iulia.florea@enea.com>
Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

src/ofp_udp6_usrreq.c
@@ -185,7 +185,7 @@ udp6_append(struct inpcb *inp, odp_packet_t pkt, int off,
 }
 
 enum ofp_return_code
-ofp_udp6_input(odp_packet_t pkt, int *offp, int *nxt)
+ofp_udp6_input(odp_packet_t *pkt, int *offp, int *nxt)
 {
 	int off = *offp;
 	int protocol = IS_IPV6_UDP;
@@ -207,20 +207,20 @@ ofp_udp6_input(odp_packet_t pkt, int *offp, int *nxt)
 #endif
 	*nxt = OFP_IPPROTO_DONE;
 
-	OFP_HOOK(OFP_HOOK_LOCAL, pkt, &protocol, &res);
+	OFP_HOOK(OFP_HOOK_LOCAL, *pkt, &protocol, &res);
 	if (res != OFP_PKT_CONTINUE)
 		return res;
 
-	OFP_HOOK(OFP_HOOK_LOCAL_UDPv6, pkt, NULL, &res);
+	OFP_HOOK(OFP_HOOK_LOCAL_UDPv6, *pkt, NULL, &res);
 	if (res != OFP_PKT_CONTINUE)
 		return res;
 
-	ifp = odp_packet_user_ptr(pkt);
-	ip6 = (struct ofp_ip6_hdr *)odp_packet_l3_ptr(pkt, NULL);
-	if (odp_packet_len(pkt) < off + sizeof(struct ofp_udphdr))
+	ifp = odp_packet_user_ptr(*pkt);
+	ip6 = (struct ofp_ip6_hdr *)odp_packet_l3_ptr(*pkt, NULL);
+	if (odp_packet_len(*pkt) < off + sizeof(struct ofp_udphdr))
 		return OFP_PKT_DROP;
 
-	odp_packet_l4_offset_set(pkt, odp_packet_l3_offset(pkt) + off);
+	odp_packet_l4_offset_set(*pkt, odp_packet_l3_offset(*pkt) + off);
 
 	uh = (struct ofp_udphdr *)((uint8_t *)ip6 + off);
 
@@ -248,7 +248,7 @@ ofp_udp6_input(odp_packet_t pkt, int *offp, int *nxt)
 		goto badunlocked;
 	}
 
-	uh_sum = ofp_in6_cksum(pkt, OFP_IPPROTO_UDP, off, ulen);
+	uh_sum = ofp_in6_cksum(*pkt, OFP_IPPROTO_UDP, off, ulen);
 	if (uh_sum != 0) {
 		UDPSTAT_INC(udps_badsum);
 		goto badunlocked;
@@ -256,7 +256,7 @@ ofp_udp6_input(odp_packet_t pkt, int *offp, int *nxt)
 	/*
 	 * Construct sockaddr format source address.
 	 */
-	ofp_init_sin6(&fromsa, pkt);
+	ofp_init_sin6(&fromsa, *pkt);
 	fromsa.sin6_port = uh->uh_sport;
 
 #if 0
@@ -485,13 +485,13 @@ ofp_udp6_input(odp_packet_t pkt, int *offp, int *nxt)
 
 	up = intoudpcb(inp);
 	if (up->u_tun_func == NULL) {
-		udp6_append(inp, pkt, off, &fromsa);
+		udp6_append(inp, *pkt, off, &fromsa);
 	} else {
 		/*
 		 * Engage the tunneling protocol.
 		 */
 
-		(*up->u_tun_func)(pkt, off, inp);
+		(*up->u_tun_func)(*pkt, off, inp);
 	}
 
 	INP_RUNLOCK(inp);

src/ofp_udp_usrreq.c
@@ -264,7 +264,7 @@ ofp_in_broadcast(struct ofp_in_addr in, struct ofp_ifnet *ifp)
 }
 
 enum ofp_return_code
-ofp_udp_input(odp_packet_t m, int off)
+ofp_udp_input(odp_packet_t *m, int off)
 {
 
 	int iphlen = off;
@@ -280,20 +280,20 @@ ofp_udp_input(odp_packet_t m, int off)
 #endif /* SP*/
 	struct ofp_sockaddr_in udp_in;
 
-	OFP_HOOK(OFP_HOOK_LOCAL, m, &protocol, &res);
+	OFP_HOOK(OFP_HOOK_LOCAL, *m, &protocol, &res);
 	if (res != OFP_PKT_CONTINUE)
 		return res;
 
-	OFP_HOOK(OFP_HOOK_LOCAL_UDPv4, m, NULL, &res);
+	OFP_HOOK(OFP_HOOK_LOCAL_UDPv4, *m, NULL, &res);
 	if (res != OFP_PKT_CONTINUE)
 		return res;
 
 	/* Offer to VXLAN handler. */
-	res = ofp_vxlan_input(m);
+	res = ofp_vxlan_input(*m);
 	if (res != OFP_PKT_CONTINUE)
 		return res;
 
-	ifp = odp_packet_user_ptr(m);
+	ifp = odp_packet_user_ptr(*m);
 	UDPSTAT_INC(udps_ipackets);
 
 	/*
@@ -310,8 +310,8 @@ ofp_udp_input(odp_packet_t m, int off)
 	/*
 	 * Get IP and UDP header together in first mbuf.
 	 */
-	ip = (struct ofp_ip *)odp_packet_l3_ptr(m, NULL);
-	if (odp_packet_len(m) < iphlen + sizeof(struct ofp_udphdr)) {
+	ip = (struct ofp_ip *)odp_packet_l3_ptr(*m, NULL);
+	if (odp_packet_len(*m) < iphlen + sizeof(struct ofp_udphdr)) {
 #if 0
 		if ((m = odp_packet_ensure_contiguous(m, iphlen +
 			      sizeof(struct ofp_udphdr))) == 0) {
@@ -378,7 +378,7 @@ ofp_udp_input(odp_packet_t m, int off)
 #if 1
 		uint16_t uh_sum;
 
-		uh_sum = ofp_in4_cksum(m);
+		uh_sum = ofp_in4_cksum(*m);
 		if (uh_sum)
 			goto badunlocked;
 #else
@@ -480,7 +480,7 @@ ofp_udp_input(odp_packet_t m, int off)
 			if (last != NULL) {
 				odp_packet_t n;
 
-				n = odp_packet_copy(m, ofp_packet_pool);
+				n = odp_packet_copy(*m, ofp_packet_pool);
 				udp_append(last, ip, n, iphlen, &udp_in);
 				INP_RUNLOCK(last);
 			}
@@ -514,7 +514,7 @@ ofp_udp_input(odp_packet_t m, int off)
 			return OFP_PKT_CONTINUE;
 #endif
 		}
-		udp_append(last, ip, m, iphlen, &udp_in);
+		udp_append(last, ip, *m, iphlen, &udp_in);
 		INP_RUNLOCK(last);
 		INP_INFO_RUNLOCK(&ofp_udbinfo);
 		return OFP_PKT_PROCESSED;
@@ -566,11 +566,11 @@ ofp_udp_input(odp_packet_t m, int off)
 	INP_RLOCK_ASSERT(inp);
 	if (inp->inp_ip_minttl && inp->inp_ip_minttl > ip->ip_ttl) {
 		INP_RUNLOCK(inp);
-		odp_packet_free(m);
+		odp_packet_free(*m);
 		return OFP_PKT_PROCESSED;
 	}
 
-	udp_append(inp, ip, m, iphlen, &udp_in);
+	udp_append(inp, ip, *m, iphlen, &udp_in);
 	INP_RUNLOCK(inp);
 	return OFP_PKT_PROCESSED;
 

include/api/ofp_pkt_processing.h
@@ -15,7 +15,7 @@
 #pragma GCC visibility push(default)
 #endif
 
-typedef enum ofp_return_code (*ofp_pkt_processing_func)(odp_packet_t pkt);
+typedef enum ofp_return_code (*ofp_pkt_processing_func)(odp_packet_t *pkt);
 
 struct ofp_ifnet;
 
@@ -30,13 +30,13 @@ uint32_t ofp_packet_min_user_area(void);
 enum ofp_return_code ofp_packet_input(odp_packet_t pkt,
 	odp_queue_t in_queue, ofp_pkt_processing_func pkt_func);
 
-enum ofp_return_code ofp_eth_vlan_processing(odp_packet_t pkt);
-enum ofp_return_code ofp_ipv4_processing(odp_packet_t pkt);
-enum ofp_return_code ofp_ipv6_processing(odp_packet_t pkt);
-enum ofp_return_code ofp_gre_processing(odp_packet_t pkt);
-enum ofp_return_code ofp_arp_processing(odp_packet_t pkt);
-enum ofp_return_code ofp_udp4_processing(odp_packet_t pkt);
-enum ofp_return_code ofp_tcp4_processing(odp_packet_t pkt);
+enum ofp_return_code ofp_eth_vlan_processing(odp_packet_t *pkt);
+enum ofp_return_code ofp_ipv4_processing(odp_packet_t *pkt);
+enum ofp_return_code ofp_ipv6_processing(odp_packet_t *pkt);
+enum ofp_return_code ofp_gre_processing(odp_packet_t *pkt);
+enum ofp_return_code ofp_arp_processing(odp_packet_t *pkt);
+enum ofp_return_code ofp_udp4_processing(odp_packet_t *pkt);
+enum ofp_return_code ofp_tcp4_processing(odp_packet_t *pkt);
 
 enum ofp_return_code ofp_send_frame(struct ofp_ifnet *dev, odp_packet_t pkt);
 enum ofp_return_code ofp_send_pending_pkt(void);

include/ofpi_gre.h
@@ -8,7 +8,7 @@
 #ifndef __OFPI_GRE_H__
 #define __OFPI_GRE_H__
 
-enum ofp_return_code ofp_gre_input(odp_packet_t, int);
+enum ofp_return_code ofp_gre_input(odp_packet_t *, int);
 
 enum ofp_return_code ofp_output_ipv4_to_gre(odp_packet_t pkt,
 					    struct ofp_ifnet *dev_gre);

include/ofpi_icmp.h
@@ -10,7 +10,7 @@
 #include "api/ofp_icmp.h"
 #include "api/ofp_types.h"
 
-enum ofp_return_code ofp_icmp_input(odp_packet_t pkt_icmp, int off);
+enum ofp_return_code ofp_icmp_input(odp_packet_t *pkt_icmp, int off);
 enum ofp_return_code ofp_icmp_error(odp_packet_t pkt_in, int type, int code, uint32_t dest, int mtu);
 
 enum ofp_return_code

include/ofpi_icmp6.h
@@ -15,7 +15,7 @@ void	icmp6_paramerror(struct mbuf *, int);
 */
 void	ofp_icmp6_error(odp_packet_t, int, int, int);
 void	ofp_icmp6_error2(odp_packet_t, int, int, int, struct ofp_ifnet *);
-enum ofp_return_code ofp_icmp6_input(odp_packet_t, int *, int *);
+enum ofp_return_code ofp_icmp6_input(odp_packet_t *, int *, int *);
 /*
 void	icmp6_fasttimo(void);
 void	icmp6_slowtimo(void);

include/ofpi_igmp_var.h
@@ -204,7 +204,7 @@ struct ofp_igmp_ifinfo *
 	ofp_igmp_domifattach(struct ofp_ifnet *);
 void	ofp_igmp_domifdetach(struct ofp_ifnet *);
 void	ofp_igmp_ifdetach(struct ofp_ifnet *);
-enum ofp_return_code ofp_igmp_input(odp_packet_t , int);
+enum ofp_return_code ofp_igmp_input(odp_packet_t *, int);
 void	ofp_igmp_slowtimo(void);
 void	ofp_igmp_init(void);
 void	ofp_igmp_uninit(void *unused);

include/ofpi_ip6_var.h
@@ -391,9 +391,9 @@ void	ofp_ip6_init (void);
 void	ofp_ip6_destroy (void);
 #endif
 
-enum ofp_return_code ofp_ip6_input(odp_packet_t, int *, int *);
-enum ofp_return_code ofp_ip6_none_input(odp_packet_t, int *, int *);
-enum ofp_return_code ofp_ip6_unrecognized_hdr_input(odp_packet_t, int *, int *);
+enum ofp_return_code ofp_ip6_input(odp_packet_t *, int *, int *);
+enum ofp_return_code ofp_ip6_none_input(odp_packet_t *, int *, int *);
+enum ofp_return_code ofp_ip6_unrecognized_hdr_input(odp_packet_t *, int *, int *);
 
 #if 0
 #ifdef _KERNEL

commit bc61b12733306a28643e423a5931893a3e29a635

Fix the way packet handle is passed to ofp_icmp[6]_error().
Pass packet handle instead of packet handle pointer to ofp_icmp_error()
and ofp_icmp6_error() in code that gets compiled when slow path has been
disabled.

Fixes: 51312a3 Fix the packet handle lost issue in case of reassembly

Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_udp6_usrreq.c
@@ -471,7 +471,7 @@ ofp_udp6_input(odp_packet_t *pkt, int *offp, int *nxt)
 #endif
 
 #ifndef SP
-		ofp_icmp6_error(pkt, OFP_ICMP6_DST_UNREACH,
+		ofp_icmp6_error(*pkt, OFP_ICMP6_DST_UNREACH,
 				OFP_ICMP6_DST_UNREACH_NOPORT, 0);
 
 		*nxt = OFP_IPPROTO_DONE;

src/ofp_udp_usrreq.c
@@ -553,7 +553,7 @@ ofp_udp_input(odp_packet_t *m, int off)
 #ifndef SP
 		*ip = save_ip;
 		ip->ip_len += iphlen;
-		ofp_icmp_error(m, OFP_ICMP_UNREACH, OFP_ICMP_UNREACH_PORT, 0, 0);
+		ofp_icmp_error(*m, OFP_ICMP_UNREACH, OFP_ICMP_UNREACH_PORT, 0, 0);
 		return OFP_PKT_PROCESSED;
 #else
 		return OFP_PKT_CONTINUE;