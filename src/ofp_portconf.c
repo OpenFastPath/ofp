OpenFastPath/ofp

commit 1532c857e475447b343549c4985d53be26a172b3

Add packet statistics to interfaces
Signed-off-by: Iulia Florea <iulia.florea@enea.com>
Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

src/ofp_portconf.c
@@ -175,13 +175,35 @@ static void ofp_vlan_free(struct ofp_ifnet *vlan)
 	odp_rwlock_write_unlock(&vlan_shm->vlan_mtx);
 }
 
+static void print_eth_stats (odp_pktio_stats_t stats, int fd)
+{
+	ofp_sendf(fd,
+		"\tRX: bytes:%lu packets:%lu dropped:%lu errors:%lu unknown:%lu\r\n",
+		stats.in_octets,
+		stats.in_ucast_pkts,
+		stats.in_discards,
+		stats.in_errors,
+		stats.in_unknown_protos);
+
+	ofp_sendf(fd,
+		"\tTX: bytes:%lu packets:%lu dropped:%lu error:%lu\r\n\r\n",
+		stats.out_octets,
+		stats.out_ucast_pkts,
+		stats.out_discards,
+		stats.out_errors);
+}
+
 static int iter_vlan(void *key, void *iter_arg)
 {
 	struct ofp_ifnet *iface = key;
 	char buf[16];
 	int fd = *((int *)iter_arg);
-
+	odp_pktio_stats_t stats;
+	int res;
 	uint32_t mask = ~0;
+
+
+	res = odp_pktio_stats(iface->pktio, &stats);
 
 	mask = odp_cpu_to_be_32(mask << (32 - iface->masklen));
 
@@ -214,9 +236,13 @@ static int iter_vlan(void *key, void *iter_arg)
 			iface->if_mtu);
 
 		ofp_sendf(fd,
-			"	Local: %s	Remote: %s\r\n\r\n",
+			"	Local: %s	Remote: %s\r\n",
 			ofp_print_ip_addr(iface->ip_local),
 			ofp_print_ip_addr(iface->ip_remote));
+		if (res == 0)
+			print_eth_stats(stats, fd);
+		else
+			ofp_sendf(fd, "\r\n");
 		return 0;
 	} else if (iface->port == GRE_PORTS && !iface->vlan) {
 		ofp_sendf(fd, "gre%d\r\n"
@@ -256,6 +282,10 @@ static int iter_vlan(void *key, void *iter_arg)
 			  ofp_port_vlan_to_ifnet_name(iface->physport,
 						      iface->physvlan),
 			  iface->if_mtu);
+		if (res == 0)
+			print_eth_stats(stats, fd);
+		else
+			ofp_sendf(fd, "\r\n");
 		return 0;
 	}
 
@@ -286,6 +316,10 @@ static int iter_vlan(void *key, void *iter_arg)
 			  iface->ip6_prefix,
 #endif /* INET6 */
 			  iface->if_mtu);
+		if (res == 0)
+			print_eth_stats(stats, fd);
+		else
+			ofp_sendf(fd, "\r\n");
 		return 0;
 	}
 
@@ -337,8 +371,12 @@ static int iter_vlan(void *key, void *iter_arg)
 #endif /* INET6 */
 
 		ofp_sendf(fd,
-			"	MTU: %d\r\n\r\n",
+			"	MTU: %d\r\n",
 			iface->if_mtu);
+		if (res == 0)
+			print_eth_stats(stats, fd);
+		else
+			printf("\r\n");
 	} else {
 		ofp_sendf(fd, "%s%d%s\r\n"
 			"	Link not configured\r\n\r\n",

src/ofp_ifnet.c
@@ -310,6 +310,8 @@ int ofp_ifnet_create(odp_instance_t instance,
 		}
 	}
 
+	odp_pktio_stats_reset(ifnet->pktio);
+
 #ifdef SP
 	/* Start VIF slowpath receiver thread */
 	thr_params.start = sp_rx_thread;

commit e459eec0ab86da8e9e3c342076481f29180b2541

Fix a cosmetic issue in interface display in CLI.
Send CRLF to the client fd instead of stdout.

Fixes: 1532c85 Add packet statistics to interfaces

Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
Reviewed-by: Sirshak Das <sirshak.das@arm.com>

src/ofp_portconf.c
@@ -376,7 +376,7 @@ static int iter_vlan(void *key, void *iter_arg)
 		if (res == 0)
 			print_eth_stats(stats, fd);
 		else
-			printf("\r\n");
+			ofp_sendf(fd, "\r\n");
 	} else {
 		ofp_sendf(fd, "%s%d%s\r\n"
 			"	Link not configured\r\n\r\n",