OpenFastPath/ofp

commit 5c48613ce7acf8e315b4c3767b39c9d08efe5952

Add UDP RX checksum validation offloading
Signed-off-by: Viktor Tikkanen <viktor.tikkanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_ifnet.c
@@ -40,7 +40,7 @@ static int ofp_pktio_config(struct ofp_ifnet *ifnet)
 		ifnet->chksum_offload_flags |= OFP_IF_IPV4_RX_CHKSUM;
 		config.pktin.bit.ipv4_chksum = 1;
 		config.pktin.bit.drop_ipv4_err = 1;
-		OFP_DBG("Interface '%s' supports IPv4 RX checksum offload\n",
+		OFP_DBG("Interface '%s' supports IPv4 RX checksum offload",
 			ifnet->if_name);
 	}
 
@@ -48,7 +48,15 @@ static int ofp_pktio_config(struct ofp_ifnet *ifnet)
 		ifnet->chksum_offload_flags |= OFP_IF_IPV4_TX_CHKSUM;
 		config.pktout.bit.ipv4_chksum = 1;
 		config.pktout.bit.ipv4_chksum_ena = 1;
-		OFP_DBG("Interface '%s' supports IPv4 TX checksum offload\n",
+		OFP_DBG("Interface '%s' supports IPv4 TX checksum offload",
+			ifnet->if_name);
+	}
+
+	if (capa.config.pktin.bit.udp_chksum) {
+		ifnet->chksum_offload_flags |= OFP_IF_UDP_RX_CHKSUM;
+		config.pktin.bit.udp_chksum = 1;
+		config.pktin.bit.drop_udp_err = 1;
+		OFP_DBG("Interface '%s' supports UDP RX checksum offload",
 			ifnet->if_name);
 	}
 

include/ofpi_pkt_processing.h
@@ -36,6 +36,7 @@ struct ip_out {
 #define OFP_IP_OUTPUT_MAX_RECURSION 8
 
 #define OFP_L3_CHKSUM_STATUS_VALID  0x1
+#define OFP_L4_CHKSUM_STATUS_VALID  0x2
 
 struct ofp_packet_user_area {
 	uint8_t recursion_count;

include/ofpi_portconf.h
@@ -168,6 +168,7 @@ struct ofp_ifnet {
 	odp_pktio_t	pktio;
 #define OFP_IF_IPV4_RX_CHKSUM 0x1
 #define OFP_IF_IPV4_TX_CHKSUM 0x2
+#define OFP_IF_UDP_RX_CHKSUM  0x4
 	uint32_t        chksum_offload_flags;
 	unsigned	out_queue_num;
 #define OFP_OUT_QUEUE_TYPE_PKTOUT 0

src/ofp_pkt_processing.c
@@ -1388,6 +1388,10 @@ enum ofp_return_code ofp_packet_input(odp_packet_t pkt,
 		ofp_packet_user_area(pkt)->chksum_flags |=
 			OFP_L3_CHKSUM_STATUS_VALID;
 
+	if (ifnet->chksum_offload_flags & OFP_IF_UDP_RX_CHKSUM)
+		ofp_packet_user_area(pkt)->chksum_flags |=
+			OFP_L4_CHKSUM_STATUS_VALID;
+
 	OFP_DEBUG_PACKET(OFP_DEBUG_PKT_RECV_NIC, pkt, ifnet->port);
 
 	OFP_UPDATE_PACKET_STAT(rx_fp, 1);

src/ofp_udp_usrreq.c
@@ -375,38 +375,24 @@ ofp_udp_input(odp_packet_t *m, int off)
 
 	if (uh->uh_sum) {
 #ifdef OFP_IPv4_UDP_CSUM_VALIDATE
-#if 1
-		uint16_t uh_sum;
-
-		uh_sum = ofp_in4_cksum(*m);
-		if (uh_sum)
+		if (ofp_packet_user_area(*m)->chksum_flags
+			& OFP_L4_CHKSUM_STATUS_VALID) {
+			switch (odp_packet_l4_chksum_status(*m)) {
+			case ODP_PACKET_CHKSUM_OK:
+				break;
+			case ODP_PACKET_CHKSUM_UNKNOWN:
+				/* Checksum was not validated by HW */
+				if (ofp_in4_cksum(*m))
+					goto badunlocked;
+				break;
+			case ODP_PACKET_CHKSUM_BAD:
+				goto badunlocked;
+				break;
+			}
+			ofp_packet_user_area(*m)->chksum_flags &=
+				~OFP_L4_CHKSUM_STATUS_VALID;
+		} else if (ofp_in4_cksum(*m))
 			goto badunlocked;
-#else
-		uint16_t uh_sum;
-
-		if (odp_packet_csum_flags(m) & CSUM_DATA_VALID) {
-			if (odp_packet_csum_flags(m) & CSUM_PSEUDO_HDR)
-				uh_sum = odp_packet_csum_data(m);
-			else
-				uh_sum = in_pseudo(ip->ip_src.s_addr,
-				    ip->ip_dst.s_addr, odp_cpu_to_be_32((uint16_t)len +
-				    odp_packet_csum_data(m) + OFP_IPPROTO_UDP));
-			uh_sum ^= 0xffff;
-		} else {
-			char b[9];
-
-			bcopy(((struct ipovly *)ip)->ih_x1, b, 9);
-			bzero(((struct ipovly *)ip)->ih_x1, 9);
-			((struct ipovly *)ip)->ih_len = uh->uh_ulen;
-			uh_sum = in_cksum(m, len + sizeof (struct ofp_ip));
-			bcopy(b, ((struct ipovly *)ip)->ih_x1, 9);
-		}
-		if (uh_sum) {
-			UDPSTAT_INC(udps_badsum);
-			odp_packet_free(m));
-			return(0);
-		}
-#endif
 #endif /*OFP_IPv4_UDP_CSUM_VALIDATE*/
 	} else {
 		UDPSTAT_INC(udps_nosum);

commit fa1cd09714ccb7d0fab7bc38bd43dd894403adb1

Do not drop forwarded packets with bad L4 checksums.
A router is not supposed to check the L4 cheksum of packets not
destined to it. Do not ask ODP to drop every packet with a bad UDP
or TCP checksum but let the transport layer code take care of the
locally destined ones.

Fixes: 5c48613 Add UDP RX checksum validation offloading
Fixes: 9157089 Add TCP RX checksum validation offloading

Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
Reviewed-by: Sirshak Das <sirshak.das@arm.com>

src/ofp_ifnet.c
@@ -55,7 +55,7 @@ static int ofp_pktio_config(struct ofp_ifnet *ifnet)
 	if (capa.config.pktin.bit.udp_chksum) {
 		ifnet->chksum_offload_flags |= OFP_IF_UDP_RX_CHKSUM;
 		config.pktin.bit.udp_chksum = 1;
-		config.pktin.bit.drop_udp_err = 1;
+		config.pktin.bit.drop_udp_err = 0;
 		OFP_DBG("Interface '%s' supports UDP RX checksum offload",
 			ifnet->if_name);
 	}
@@ -75,7 +75,7 @@ static int ofp_pktio_config(struct ofp_ifnet *ifnet)
 	if (capa.config.pktin.bit.tcp_chksum) {
 		ifnet->chksum_offload_flags |= OFP_IF_TCP_RX_CHKSUM;
 		config.pktin.bit.tcp_chksum = 1;
-		config.pktin.bit.drop_tcp_err = 1;
+		config.pktin.bit.drop_tcp_err = 0;
 		OFP_DBG("Interface '%s' supports TCP RX checksum offload",
 			ifnet->if_name);
         }