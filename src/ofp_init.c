OpenFastPath/ofp

commit d683597d43fdcd6d42fe20bdd071733827e5698d

Remove the malloc from port configuration function
This is part of the fix for issue 67. Malloc is used in several places when creating data that can be used from different cores. To avoid this, the allocation has been changed to use shared memory instead. The first step was to remove this from port configuration functions.

Signed-off-by: Iulia Florea <iulia.florea@enea.com>
Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

src/ofp_init.c
@@ -151,6 +151,8 @@ int ofp_init_pre_global(const char *pool_name_unused,
 
 	HANDLE_ERROR(ofp_portconf_init_global());
 
+	HANDLE_ERROR(ofp_vlan_init_global());
+
 	HANDLE_ERROR(ofp_vxlan_init_global());
 
 	odp_pool_param_t pool_params;
@@ -242,6 +244,7 @@ int ofp_init_local(void)
 	HANDLE_ERROR(ofp_uma_lookup_shared_memory());
 	HANDLE_ERROR(ofp_global_config_lookup_shared_memory());
 	HANDLE_ERROR(ofp_portconf_lookup_shared_memory());
+	HANDLE_ERROR(ofp_vlan_lookup_shared_memory());
 	HANDLE_ERROR(ofp_route_lookup_shared_memory());
 	HANDLE_ERROR(ofp_avl_lookup_shared_memory());
 	HANDLE_ERROR(ofp_reassembly_lookup_shared_memory());
@@ -376,6 +379,7 @@ int ofp_term_post_global(const char *pool_name)
 	CHECK_ERROR(ofp_vxlan_term_global(), rc);
 
 	/* Cleanup interface related objects */
+	CHECK_ERROR(ofp_vlan_term_global(), rc);
 	CHECK_ERROR(ofp_portconf_term_global(), rc);
 
 	/* Cleanup routes */

include/api/ofp_config.h
@@ -112,6 +112,9 @@
  * For each fastpath interface a PKTIO in opened by OFP.*/
 #define OFP_FP_INTERFACE_MAX 8
 
+/* Maximum numbers of VLANs */
+#define OFP_NUM_VLAN_MAX 256
+
 /**Maximum number of input queues that can be configured for an
    * OFP interface*/
 #define OFP_PKTIN_QUEUE_MAX 64

include/api/ofp_portconf.h
@@ -46,6 +46,7 @@ int ofp_get_num_ports(void);
 struct ofp_ifnet *ofp_get_ifnet(int port, uint16_t vlan);
 struct ofp_ifnet *ofp_get_create_ifnet(int port, uint16_t vlan);
 int ofp_delete_ifnet(int port, uint16_t vlan);
+struct ofp_ifnet *ofp_vlan_alloc(void);
 
 odp_pktio_t ofp_port_pktio_get(int port);
 

include/ofpi_portconf.h
@@ -197,6 +197,7 @@ struct ofp_ifnet {
 	struct ofp_in_ifinfo ii_inet;
 	void	*if_afdata[OFP_AF_MAX];
 	struct	ofp_ifmultihead if_multiaddrs; /* multicast addresses configured */
+	struct ofp_ifnet *next;	/* next in the free list */
 };
 
 #define outq_def out_queue_queue[0]
@@ -346,6 +347,9 @@ int ofp_free_port_alloc(void);
 int ofp_portconf_lookup_shared_memory(void);
 int ofp_portconf_init_global(void);
 int ofp_portconf_term_global(void);
+int ofp_vlan_lookup_shared_memory(void);
+int ofp_vlan_init_global(void);
+int ofp_vlan_term_global(void);
 
 #ifdef SP
 void ofp_update_ifindex_lookup_tab(struct ofp_ifnet *ifnet);

src/ofp_netlink.c
@@ -491,7 +491,7 @@ static int add_link(struct ifinfomsg *ifinfo_entry, int vlan, int link,
 			&key,
 			(void **)&dev)) {
 
-			dev = malloc(sizeof(struct ofp_ifnet));
+			dev = ofp_vlan_alloc();
 			memset(dev, 0, sizeof(struct ofp_ifnet));
 			dev->port = dev_root->port;
 			dev->vlan = vlan;

src/ofp_portconf.c
@@ -30,6 +30,8 @@
 
 #define SHM_NAME_PORTS "OfpPortconfShMem"
 #define SHM_NAME_PORT_LOCKS "OfpPortconfLocksShMem"
+#define SHM_NAME_VLAN "OfpVlanconfShMem"
+
 
 #ifdef SP
 #define NUM_LINUX_INTERFACES 512
@@ -58,12 +60,19 @@ struct ofp_portconf_mem {
 #endif /* SP */
 };
 
+struct ofp_vlan_mem {
+	struct ofp_ifnet *free_ifnet_list;
+	odp_rwlock_t vlan_mtx;
+	struct ofp_ifnet vlan_ifnet[0];
+};
 
 /*
  * Data per core
  */
 static __thread struct ofp_portconf_mem *shm;
 struct ofp_ifnet_locks_str  *ofp_ifnet_locks_shm;
+
+static __thread struct ofp_vlan_mem *vlan_shm;
 
 /*Wrapper functions over AVL tree*/
 static void *new_vlan(
@@ -139,6 +148,31 @@ int ofp_free_port_alloc(void)
 		return -1;
 	}
 	return port;
+}
+
+struct ofp_ifnet *ofp_vlan_alloc(void)
+{
+	odp_rwlock_write_lock(&vlan_shm->vlan_mtx);
+	struct ofp_ifnet *vlan = vlan_shm->free_ifnet_list;
+	if (vlan_shm->free_ifnet_list) {
+		vlan_shm->free_ifnet_list = vlan_shm->free_ifnet_list->next;
+	}
+	odp_rwlock_write_unlock(&vlan_shm->vlan_mtx);
+
+	if (vlan == NULL) {
+		OFP_ERR("Cannot allocate vlan!");
+		return (NULL);
+	}
+
+	return vlan;
+}
+
+static void ofp_vlan_free(struct ofp_ifnet *vlan)
+{
+	odp_rwlock_write_lock(&vlan_shm->vlan_mtx);
+	vlan->next = vlan_shm->free_ifnet_list;
+	vlan_shm->free_ifnet_list = vlan;
+	odp_rwlock_write_unlock(&vlan_shm->vlan_mtx);
 }
 
 static int iter_vlan(void *key, void *iter_arg)
@@ -355,7 +389,7 @@ void ofp_show_interfaces(int fd)
 
 int free_key(void *key)
 {
-	free(key);
+	ofp_vlan_free(key);
 	return 1;
 }
 
@@ -1162,7 +1196,7 @@ struct ofp_ifnet *ofp_get_create_ifnet(int port, uint16_t vlan)
 				shm->ofp_ifnet_data[port].vlan_structs,
 				&key,
 				(void *)&data)) {
-			data = malloc(sizeof(*data));
+			data = ofp_vlan_alloc();
 			memset(data, 0, sizeof(*data));
 			data->port = port;
 			data->vlan = vlan;
@@ -1499,6 +1533,16 @@ static int ofp_portconf_alloc_shared_memory(void)
 	return 0;
 }
 
+static int ofp_vlan_alloc_shared_memory(void)
+{
+	vlan_shm = ofp_shared_memory_alloc(SHM_NAME_VLAN, sizeof(struct ofp_vlan_mem) + sizeof(struct ofp_ifnet)*OFP_NUM_VLAN_MAX);
+	if (vlan_shm == NULL) {
+		OFP_ERR("ofp_shared_memory_alloc failed");
+		return -1;
+	}
+	return 0;
+}
+
 static int ofp_portconf_free_shared_memory(void)
 {
 	int rc = 0;
@@ -1517,6 +1561,20 @@ static int ofp_portconf_free_shared_memory(void)
 	return rc;
 }
 
+
+static int ofp_vlan_free_shared_memory(void)
+{
+	int rc = 0;
+
+	if (ofp_shared_memory_free(SHM_NAME_VLAN) == -1) {
+		OFP_ERR("ofp_shared_memory_free failed");
+		rc = -1;
+	}
+	vlan_shm = NULL;
+	return rc;
+}
+
+
 int ofp_portconf_lookup_shared_memory(void)
 {
 	shm = ofp_shared_memory_lookup(SHM_NAME_PORTS);
@@ -1531,6 +1589,17 @@ int ofp_portconf_lookup_shared_memory(void)
 		return -1;
 	}
 
+	return 0;
+}
+
+
+int ofp_vlan_lookup_shared_memory(void)
+{
+	vlan_shm = ofp_shared_memory_lookup(SHM_NAME_VLAN);
+	if (vlan_shm == NULL) {
+		OFP_ERR("ofp_shared_memory_lookup failed");
+		return -1;
+	}
 	return 0;
 }
 
@@ -1616,6 +1685,23 @@ int ofp_portconf_init_global(void)
 	return 0;
 }
 
+int ofp_vlan_init_global(void)
+{
+	int i;
+
+	/* init vlan shared memory */
+	HANDLE_ERROR(ofp_vlan_alloc_shared_memory());
+	memset(vlan_shm, 0, sizeof(*vlan_shm));
+	for (i = 0; i < OFP_NUM_VLAN_MAX; i++) {
+		vlan_shm->vlan_ifnet[i].next = (i == OFP_NUM_VLAN_MAX - 1) ?
+			NULL : &(vlan_shm->vlan_ifnet[i+1]);
+	}
+	vlan_shm->free_ifnet_list = &(vlan_shm->vlan_ifnet[0]);
+	odp_rwlock_init(&vlan_shm->vlan_mtx);
+
+	return 0;
+}
+
 int ofp_portconf_term_global(void)
 {
 	int i;
@@ -1635,6 +1721,22 @@ int ofp_portconf_term_global(void)
 
 	return rc;
 }
+
+
+int ofp_vlan_term_global(void)
+{
+	int rc = 0;
+
+	vlan_shm = ofp_shared_memory_lookup(SHM_NAME_VLAN);
+	if (vlan_shm == NULL) {
+		OFP_ERR("ofp_shared_memory_lookup failed");
+		rc = -1;
+	}
+	CHECK_ERROR(ofp_vlan_free_shared_memory(), rc);
+
+	return rc;
+}
+
 
 struct ofp_in_ifaddrhead *ofp_get_ifaddrhead(void)
 {

commit b5af3c1bb98720acf1079b68412ee946893fd8ca

Fix the order of ofp_vlan_term_global() and ofp_portconf_term_global().
ofp_vlan_term_global() must be done after ofp_portconf_term_global() to
avoid a segfault at OFP termination when there are VLAN or GRE interfaces.
ofp_portconf_term_global() performs VLAN deletions which require that
the VLAN SHM still exists.

Switch also the order of the corresponding init_global functions for
symmetry.

Fixes: d683597 Remove the malloc from port configuration function

Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_init.c
@@ -366,9 +366,9 @@ static int ofp_init_pre_global(ofp_global_param_t *params)
 
 	HANDLE_ERROR(ofp_route_init_global());
 
-	HANDLE_ERROR(ofp_portconf_init_global());
-
 	HANDLE_ERROR(ofp_vlan_init_global());
+
+	HANDLE_ERROR(ofp_portconf_init_global());
 
 	HANDLE_ERROR(ofp_vxlan_init_global());
 
@@ -606,8 +606,8 @@ int ofp_term_post_global(const char *pool_name)
 	CHECK_ERROR(ofp_vxlan_term_global(), rc);
 
 	/* Cleanup interface related objects */
-	CHECK_ERROR(ofp_vlan_term_global(), rc);
 	CHECK_ERROR(ofp_portconf_term_global(), rc);
+	CHECK_ERROR(ofp_vlan_term_global(), rc);
 
 	/* Cleanup routes */
 	CHECK_ERROR(ofp_route_term_global(), rc);