OpenFastPath/ofp

commit 082cacdf4e98ee4418e45acb2c8cb5057d57263f

Optimize ofp_cksum_buffer().
Optimize ofp_cksum_buffer() by summing four bytes at a time instead of
two, and by unrolling the loop.

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_in_cksum.c
@@ -63,35 +63,70 @@ uint16_t ofp_cksum_iph(const void *addr, int ip_hl)
 	return ~ofp_cksum_fold(sum);
 }
 
-uint16_t ofp_cksum_buffer(uint16_t *addr, int len)
+uint16_t ofp_cksum_buffer(const void *addr, int len)
 {
 	register int nleft = len;
-	register uint16_t *w = addr;
-	register uint16_t answer;
-	register int sum = 0;
+	register uint64_t sum = 0;
 
-	/*
-	 *  Our algorithm is simple, using a 32 bit accumulator (sum),
-	 *  we add sequential 16 bit words to it, and at the end, fold
-	 *  back all the carry bits from the top 16 bits into the lower
-	 *  16 bits.
-	 */
-	while (nleft > 1)  {
+	const uint16_t *w = (const uint16_t *)addr;
+
+	if ((uint64_t)w & 2 && nleft >= 2) {
 		sum += *w++;
 		nleft -= 2;
 	}
 
-	/* mop up an odd byte, if necessary */
-	if (nleft == 1)
-		sum += odp_cpu_to_be_16(*(u_char *)w << 8);
+	register const uint32_t *d = (const uint32_t *)w;
 
+#ifdef __ARM_ARCH
 	/*
-	 * add back carry outs from top 16 bits to low 16 bits
+	 * On ARM the main loop compiles into ldp (load pair)
+	 * instructions, so we need to align to the size of a pair of
+	 * dwords, or 8 bytes.
 	 */
-	sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16 */
-	sum += (sum >> 16);                 /* add carry */
-	answer = ~sum;                      /* truncate to 16 bits */
-	return answer;
+	if ((uint64_t)d & 4 && nleft >= 4) {
+		sum += *d++;
+		nleft -= 4;
+	}
+#endif
+
+	while (nleft >= 32)  {
+		sum += *d++;
+		sum += *d++;
+		sum += *d++;
+		sum += *d++;
+
+		sum += *d++;
+		sum += *d++;
+		sum += *d++;
+		sum += *d++;
+
+		nleft -= 32;
+	}
+
+	switch (nleft>>2) {
+	case 7: sum += *d++;
+	case 6: sum += *d++;
+	case 5: sum += *d++;
+	case 4: sum += *d++;
+	case 3: sum += *d++;
+	case 2: sum += *d++;
+	case 1: sum += *d++;
+	default: break;
+	}
+
+	nleft &= 3;
+
+	w = (const uint16_t *)d;
+
+	if (nleft > 1)  {
+		sum += *w++;
+		nleft -= 2;
+	}
+
+	if (nleft == 1)
+		sum += odp_cpu_to_be_16(*(const uint8_t *)w << 8);
+
+	return ~ofp_cksum_fold(sum);
 }
 
 #define ADDCARRY(x)  (x > 65535 ? x -= 65535 : x)
@@ -126,7 +161,7 @@ static int __ofp_cksum(const odp_packet_t pkt, unsigned int off,
 			cksum_len = len;
 
 		cksum_data = (uint8_t *)odp_packet_seg_data(pkt, seg) + off;
-		tmp = ~ofp_cksum_buffer((uint16_t *)cksum_data, cksum_len);
+		tmp = ~ofp_cksum_buffer(cksum_data, cksum_len);
 
 		/* swap bytes on odd boundary */
 		if (done % 2)

include/api/ofp_in.h
@@ -558,7 +558,16 @@ char	*ofp_inet_ntoa(struct ofp_in_addr); /* implement */
  */
 uint16_t ofp_cksum_iph(const void *addr, int ip_hl);
 
-uint16_t ofp_cksum_buffer(uint16_t *addr, int len);
+/**
+ * Calculate checksum for IP, UDP or TCP.
+ *
+ * @param addr Pointer to the data. This should be 2-byte aligned,
+ *             otherwise performance may suffer.
+ * @param len  Number of bytes of data.
+ * @return Checksum.
+ */
+uint16_t ofp_cksum_buffer(const void *addr, int len);
+
 int ofp_cksum(const odp_packet_t pkt, unsigned int off, unsigned int len);
 int ofp_in4_cksum(const odp_packet_t pkt);
 

src/ofp_igmp.c
@@ -1539,7 +1539,7 @@ ofp_igmp_input(odp_packet_t *m, int off)
 	/*
 	 * Validate checksum.
 	 */
-	if (ofp_cksum_buffer((uint16_t *)igmp, igmplen)) {
+	if (ofp_cksum_buffer(igmp, igmplen)) {
 		IGMPSTAT_INC(igps_rcv_badsum);
 		return OFP_PKT_DROP;
 	}
@@ -2260,8 +2260,7 @@ igmp_v1v2_queue_report(struct ofp_in_multi *inm, const int type)
 	igmp->igmp_code = 0;
 	igmp->igmp_group = inm->inm_addr;
 	igmp->igmp_cksum = 0;
-	igmp->igmp_cksum = ofp_cksum_buffer((uint16_t *)igmp,
-			sizeof(struct igmp));
+	igmp->igmp_cksum = ofp_cksum_buffer(igmp, sizeof(struct igmp));
 
 	ip->ip_tos = 0;
 	ip->ip_len = sizeof(struct ofp_ip) + sizeof(struct igmp);
@@ -3515,7 +3514,7 @@ igmp_v3_encap_report(struct ofp_ifnet *ifp, odp_packet_t m)
 	igmp->ir_rsv2 = 0;
 	igmp->ir_numgrps = odp_cpu_to_be_16(PKT2HDR(m)->vt_nrecs);
 	igmp->ir_cksum = 0;
-	igmp->ir_cksum = ofp_cksum_buffer((uint16_t *)igmp,
+	igmp->ir_cksum = ofp_cksum_buffer(igmp,
 			sizeof(struct igmp_report) + igmpreclen);
 	PKT2HDR(m)->vt_nrecs = 0;
 

src/ofp_nd6.c
@@ -132,8 +132,7 @@ enum ofp_return_code ofp_nd6_ns_output(struct ofp_ifnet *dev,
 	icmp->ofp_icmp6_data8[21] = 1; /* 8 octets */
 	memcpy(&icmp->ofp_icmp6_data8[22], dev->mac, 6);
 
-	icmp->icmp6_cksum =
-		ofp_cksum_buffer((uint16_t *)&ip6hdr->ofp_ip6_plen, 68);
+	icmp->icmp6_cksum = ofp_cksum_buffer(&ip6hdr->ofp_ip6_plen, 68);
 
 	ip6hdr->ofp_ip6_nxt = OFP_IPPROTO_ICMPV6;
 	ip6hdr->ofp_ip6_hlim = 255;
@@ -261,8 +260,7 @@ enum ofp_return_code ofp_nd6_na_output(struct ofp_ifnet *dev,
 	icmp->ofp_icmp6_data8[21] = 1; /* 8 octets */
 	memcpy(&icmp->ofp_icmp6_data8[22], dev->mac, 6);
 
-	icmp->icmp6_cksum =
-		ofp_cksum_buffer((uint16_t *)&ip6hdr->ofp_ip6_plen, 68);
+	icmp->icmp6_cksum = ofp_cksum_buffer(&ip6hdr->ofp_ip6_plen, 68);
 
 	ip6hdr->ofp_ip6_nxt = OFP_IPPROTO_ICMPV6;
 	ip6hdr->ofp_ip6_hlim = 255;

commit 2b5df8b4f9a34ede1f9565470c9d9635899ba9de

Fix fallthrough warnings.
gcc-7 -Wimplicit-fallthrough (included in -Wextra) warns about switch
cases falling through to the next case. Get rid of the warnings by
indicating intentional fallthrough.

Fixes: 082cacd Optimize ofp_cksum_buffer().
Fixes: 15d7f92 Implement a separate IPv4 header checksum function.

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Sirshak Das<sirshak.das@arm.com>

src/ofp_in_cksum.c
@@ -42,17 +42,17 @@ uint16_t ofp_cksum_iph(const void *addr, int ip_hl)
 
 	if (odp_unlikely(ip_hl)) {
 		switch (ip_hl) {
-		case 11: sum += *d++;
-		case 10: sum += *d++;
-		case 9: sum += *d++;
-		case 8: sum += *d++;
-		case 7: sum += *d++;
-		case 6: sum += *d++;
-		case 5: sum += *d++;
-		case 4: sum += *d++;
-		case 3: sum += *d++;
-		case 2: sum += *d++;
-		case 1: sum += *d++;
+		case 11: sum += *d++; /* FALLTHROUGH */
+		case 10: sum += *d++; /* FALLTHROUGH */
+		case 9: sum += *d++; /* FALLTHROUGH */
+		case 8: sum += *d++; /* FALLTHROUGH */
+		case 7: sum += *d++; /* FALLTHROUGH */
+		case 6: sum += *d++; /* FALLTHROUGH */
+		case 5: sum += *d++; /* FALLTHROUGH */
+		case 4: sum += *d++; /* FALLTHROUGH */
+		case 3: sum += *d++; /* FALLTHROUGH */
+		case 2: sum += *d++; /* FALLTHROUGH */
+		case 1: sum += *d++; /* FALLTHROUGH */
 		default: break;
 		}
 	}
@@ -104,13 +104,13 @@ uint16_t ofp_cksum_buffer(const void *addr, int len)
 	}
 
 	switch (nleft>>2) {
-	case 7: sum += *d++;
-	case 6: sum += *d++;
-	case 5: sum += *d++;
-	case 4: sum += *d++;
-	case 3: sum += *d++;
-	case 2: sum += *d++;
-	case 1: sum += *d++;
+	case 7: sum += *d++; /* FALLTHROUGH */
+	case 6: sum += *d++; /* FALLTHROUGH */
+	case 5: sum += *d++; /* FALLTHROUGH */
+	case 4: sum += *d++; /* FALLTHROUGH */
+	case 3: sum += *d++; /* FALLTHROUGH */
+	case 2: sum += *d++; /* FALLTHROUGH */
+	case 1: sum += *d++; /* FALLTHROUGH */
 	default: break;
 	}
 