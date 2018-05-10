OpenFastPath/ofp

commit 7fd7fc1247ad7e4ed3690f820e24dff3c787d897

Use a local copy of global_param->evt_rx_burst_size in default_event_dispatcher().
Instead of fetching evt_rx_burst_size from global_param for each
burst, make and use a local copy, which is a little faster.

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_pkt_processing.c
@@ -70,13 +70,14 @@ int default_event_dispatcher(void *arg)
 	int event_cnt = 0;
 	ofp_pkt_processing_func pkt_func = (ofp_pkt_processing_func)arg;
 	odp_bool_t *is_running = NULL;
+	int rx_burst = global_param->evt_rx_burst_size;
 
 	if (ofp_init_local()) {
 		OFP_ERR("ofp_init_local failed");
 		return -1;
 	}
 
-	odp_event_t events[global_param->evt_rx_burst_size];
+	odp_event_t events[rx_burst];
 
 	is_running = ofp_get_processing_state();
 	if (is_running == NULL) {
@@ -88,7 +89,7 @@ int default_event_dispatcher(void *arg)
 	/* PER CORE DISPATCHER */
 	while (*is_running) {
 		event_cnt = odp_schedule_multi(&in_queue, ODP_SCHED_WAIT,
-					 events, global_param->evt_rx_burst_size);
+					 events, rx_burst);
 		for (event_idx = 0; event_idx < event_cnt; event_idx++) {
 			odp_event_type_t ev_type;
 

commit f7ba7a70cfc848e0db715af7276bb085444f627c

Fix a segfault in default_event_dispatcher().
Dereference global_param only after ofp_init_local(), which
initializes global_param.

Fixes: 7fd7fc1 Use a local copy of global_param->evt_rx_burst_size in default_event_dispatcher().
Fixes: #203 Segmentation fault in default_event_dispatcher

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_pkt_processing.c
@@ -70,13 +70,13 @@ int default_event_dispatcher(void *arg)
 	int event_cnt = 0;
 	ofp_pkt_processing_func pkt_func = (ofp_pkt_processing_func)arg;
 	odp_bool_t *is_running = NULL;
-	int rx_burst = global_param->evt_rx_burst_size;
 
 	if (ofp_init_local()) {
 		OFP_ERR("ofp_init_local failed");
 		return -1;
 	}
 
+	int rx_burst = global_param->evt_rx_burst_size;
 	odp_event_t events[rx_burst];
 
 	is_running = ofp_get_processing_state();