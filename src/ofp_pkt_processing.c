2fee1754e8f17fa53a419025296c36b59178cc2a
Janne Peltonen <janne.peltonen@nokia.com>
2018-07-23 16:33:13 +0300

    Move L3 offset fixing and odata.ip init out of route finding.
    Also, assume in the moved code that setting the L3 offset of an ODP packet
    to zero always succeeds.
    
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Valentin Radulescu <valentin.radulescu@enea.com>

--- src/ofp_pkt_processing.c
+++ src/ofp_pkt_processing.c
@@ -998,18 +998,9 @@ static inline enum ofp_return_code ofp_ip_output_send(odp_packet_t pkt,
 	}
 }
 
-static enum ofp_return_code ofp_ip_output_find_route(odp_packet_t pkt,
-						     struct ip_out *odata)
+static enum ofp_return_code ofp_ip_output_find_route(struct ip_out *odata)
 {
 	uint32_t flags;
-
-	if (odp_packet_l3_offset(pkt) == ODP_PACKET_OFFSET_INVALID)
-		odp_packet_l3_offset_set(pkt, 0);
-	odata->ip = (struct ofp_ip *) odp_packet_l3_ptr(pkt, NULL);
-	if (odp_unlikely(odata->ip == NULL)) {
-		OFP_DBG("ip is NULL");
-		return OFP_PKT_DROP;
-	}
 
 	if (!odata->nh) {
 		odata->nh = ofp_get_next_hop(odata->vrf, odata->ip->ip_dst.s_addr, &flags);
@@ -1119,6 +1110,7 @@ static inline enum ofp_return_code ofp_ip_output_common_inline(odp_packet_t pkt,
 	struct ofp_ifnet *send_ctx = odp_packet_user_ptr(pkt);
 	struct ip_out odata;
 	enum ofp_return_code ret;
+	struct ofp_ip *ip;
 
 	OFP_HOOK(OFP_HOOK_OUT_IPv4, pkt, NULL, &ret);
 	if (ret != OFP_PKT_CONTINUE) {
@@ -1126,13 +1118,19 @@ static inline enum ofp_return_code ofp_ip_output_common_inline(odp_packet_t pkt,
 		return ret;
 	}
 
+	ip = odp_packet_l3_ptr(pkt, NULL);
+	if (odp_unlikely(ip == NULL)) {
+		odp_packet_l3_offset_set(pkt, 0);
+		ip = odp_packet_l3_ptr(pkt, NULL);
+	}
+	odata.ip = ip;
 	odata.dev_out = NULL;
 	odata.vrf = send_ctx ? send_ctx->vrf : 0;
 	odata.is_local_address = 0;
 	odata.nh = nh_param;
 	odata.insert_checksum = is_local_out;
 
-	if ((ret = ofp_ip_output_find_route(pkt, &odata)) != OFP_PKT_CONTINUE)
+	if ((ret = ofp_ip_output_find_route(&odata)) != OFP_PKT_CONTINUE)
 		return ret;
 
 	if (is_local_out)
