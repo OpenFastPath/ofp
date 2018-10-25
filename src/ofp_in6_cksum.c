OpenFastPath/ofp

commit cc24e8ae3402dc9738d55ca914dba86a74693e80

Clean up checksum functions.
Remove ofp_getsum(), which is just a bitwise not of ofp_cksum(). It's
better if the caller does the not operation so that we don't need an
extra call level or more inlining.

Remove the static functions __ofp_cksum() and __ofp_in4_cksum().
__ofp_cksum() is not explicitly inlined, so removing it probably saves
us one call level.

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Sorin Vultureanu <sorin.vultureanu@enea.com>

src/ofp_in6_cksum.c
@@ -143,7 +143,6 @@ int ofp_in6_cksum_pseudo(struct ofp_ip6_hdr *ip6,
 int ofp_in6_cksum(odp_packet_t m, uint8_t nxt, uint32_t off, uint32_t len)
 {
 	int sum;
-	int tmp;
 	union {
 		uint16_t s[2];
 		uint32_t l;
@@ -154,9 +153,8 @@ int ofp_in6_cksum(odp_packet_t m, uint8_t nxt, uint32_t off, uint32_t len)
 	sum  = _ofp_in6_cksum_pseudo(ip6, len, nxt, 0);
 
 /* Payload*/
-	tmp = ofp_getsum(m, odp_packet_l3_offset(m) +
+	sum += ~ofp_cksum(m, odp_packet_l3_offset(m) +
 			off, len);
-	sum += tmp;
 
 	REDUCE;
 	return (~sum & 0xffff);

include/ofpi_in.h
@@ -69,6 +69,4 @@ struct __msfilterreq {
 						 */
 };
 
-int ofp_getsum(const odp_packet_t pkt, unsigned int off, unsigned int len);
-
 #endif /* __OFPI_IN_H__ */

src/ofp_in_cksum.c
@@ -134,8 +134,7 @@ uint16_t ofp_cksum_buffer(const void *addr, int len)
 l_util.l = sum; sum = l_util.s[0] + l_util.s[1]; ADDCARRY(sum);	\
 } while (0)
 
-static int __ofp_cksum(const odp_packet_t pkt, unsigned int off,
-			 unsigned int len)
+uint16_t ofp_cksum(const odp_packet_t pkt, unsigned int off, unsigned int len)
 {
 	int sum = 0;
 	uint16_t tmp = 0;
@@ -178,17 +177,7 @@ static int __ofp_cksum(const odp_packet_t pkt, unsigned int off,
 	}
 
 	REDUCE;
-	return sum;
-}
-
-uint16_t ofp_cksum(const odp_packet_t pkt, unsigned int off, unsigned int len)
-{
-	return (~__ofp_cksum(pkt, off, len)) & 0xffff;
-}
-
-int ofp_getsum(const odp_packet_t pkt, unsigned int off, unsigned int len)
-{
-	return __ofp_cksum(pkt, off, len);
+	return ~sum;
 }
 
 struct ofp_ipovly {
@@ -199,7 +188,7 @@ struct ofp_ipovly {
 	struct   ofp_in_addr ih_dst;       /* destination internet address */
 } __attribute__((__packed__));
 
-static inline int __ofp_in4_cksum(const odp_packet_t pkt)
+uint16_t ofp_in4_cksum(const odp_packet_t pkt)
 {
 	struct ofp_ip *ip;
 	int off, len, sum = 0;
@@ -232,9 +221,3 @@ static inline int __ofp_in4_cksum(const odp_packet_t pkt)
 	REDUCE;
 	return (~sum & 0xffff);
 }
-
-uint16_t ofp_in4_cksum(const odp_packet_t pkt)
-{
-	return __ofp_in4_cksum(pkt);
-}
-

test/cunit/ofp_test_in_cksum.c
@@ -176,31 +176,6 @@ test_ofp_cksum_buffer_odd_len_icmp(void)
 }
 
 static void
-test___ofp_cksum(void)
-{
-	odp_packet_t pkt;
-	struct ofp_ip *ip;
-	struct ofp_icmp *icmp;
-	uint16_t res, ip_hl;
-
-	if (create_odp_packet_ip4(&pkt, pkt1_full, sizeof(pkt1_full))) {
-		CU_FAIL("Fail to create packet");
-		return;
-	}
-
-	ip = odp_packet_l3_ptr(pkt, NULL);
-	ip_hl = ip->ip_hl << 2;
-	icmp = (struct ofp_icmp *)((uint8_t *)ip + ip_hl);
-	icmp->icmp_cksum = 0;
-
-	res = __ofp_cksum(pkt,
-			    odp_packet_l3_offset(pkt) + ip_hl,
-			    odp_be_to_cpu_16(ip->ip_len) - ip_hl);
-
-	CU_ASSERT_EQUAL(res, 0xA8ED);
-}
-
-static void
 test_ofp_cksum(void)
 {
 	odp_packet_t pkt;
@@ -303,10 +278,6 @@ main(void)
 	}
 	if (NULL == CU_ADD_TEST(ptr_suite,
 				test_ofp_cksum_buffer_odd_len_icmp)) {
-		CU_cleanup_registry();
-		return CU_get_error();
-	}
-	if (NULL == CU_ADD_TEST(ptr_suite, test___ofp_cksum)) {
 		CU_cleanup_registry();
 		return CU_get_error();
 	}

commit 9167463733e1c15a7dc92a44d81b99720dd0b231

Fix IPv6 checksum calculation.
Handle integer promotion of a partial sum in ofp_in6_cksum().

Fixes: cc24e8a Clean up checksum functions.
Fixes: #215 IPv6 broken

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Valentin Radulescu <valentin.radulescu@enea.com>

src/ofp_in6_cksum.c
@@ -143,6 +143,7 @@ int ofp_in6_cksum_pseudo(struct ofp_ip6_hdr *ip6,
 int ofp_in6_cksum(odp_packet_t m, uint8_t nxt, uint32_t off, uint32_t len)
 {
 	int sum;
+	uint16_t tmp;
 	union {
 		uint16_t s[2];
 		uint32_t l;
@@ -153,8 +154,9 @@ int ofp_in6_cksum(odp_packet_t m, uint8_t nxt, uint32_t off, uint32_t len)
 	sum  = _ofp_in6_cksum_pseudo(ip6, len, nxt, 0);
 
 /* Payload*/
-	sum += ~ofp_cksum(m, odp_packet_l3_offset(m) +
+	tmp = ~ofp_cksum(m, odp_packet_l3_offset(m) +
 			off, len);
+	sum += tmp;
 
 	REDUCE;
 	return (~sum & 0xffff);