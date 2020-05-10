e38b8d10cefdb1386f3b1efe069496ff70ae262e
Bogdan Pricope <bogdan.pricope.2013@gmail.com>
2020-05-10 14:01:01 +0300

    scripts: update devbuild script
    Script should use the supported dpdk and odp-dpdk versions
    for the current version of ofp.
    
    Signed-off-by: Bogdan Pricope <bogdan.pricope.2013@gmail.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- scripts/devbuild_ofp_odp_dpdk.sh
+++ scripts/devbuild_ofp_odp_dpdk.sh
@@ -15,7 +15,7 @@ echo '#include "pcap.h"' | cpp -H -o /dev/null 2>&1 || \
 echo '#include "numa.h"' | cpp -H -o /dev/null 2>&1 || \
     echo "Warning: NUMA library is not installed. You need to install libnuma-dev"
 
-git -c advice.detachedHead=false clone -q --depth=1 --branch=17.11 http://dpdk.org/git/dpdk-stable dpdk
+git -c advice.detachedHead=false clone -q --depth=1 --branch=18.11 http://dpdk.org/git/dpdk-stable dpdk
 pushd dpdk
 git log --oneline --decorate
 
@@ -27,21 +27,20 @@ sed -ri 's,(CONFIG_RTE_LIBRTE_PMD_PCAP=).*,\1y,' .config
 popd
 
 #Build DPDK
-make -j${JOBS} build O=${TARGET} EXTRA_CFLAGS="-fPIC"
-make install O=${TARGET} DESTDIR=${TARGET}
+make -j${JOBS} install T=${TARGET} DESTDIR=./install EXTRA_CFLAGS="-fPIC"
 popd
 
 # Clone odp-dpdk
-git clone -q https://github.com/Linaro/odp-dpdk
+git clone -q https://github.com/OpenDataPlane/odp-dpdk
 pushd odp-dpdk
-git checkout v1.19.0.2_DPDK_17.11
+git checkout -b v1.23.1 d25d47b3c5b1dcde440960c259d5066c2dec0a2d
 
 export CONFIGURE_FLAGS="--enable-shared=yes --enable-helper-linux"
 
 #Build ODP
 ./bootstrap
 ./configure  --enable-debug --enable-debug-print \
-	     --with-dpdk-path=`pwd`/../dpdk/${TARGET}/usr/local --prefix=$(pwd)/install
+	     --with-dpdk-path=`pwd`/../dpdk/install --prefix=$(pwd)/install
 make -j${JOBS} install
 popd
 
