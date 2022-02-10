21899ca4691916be01397d4b478811448b4877ad
Matias Elo <matias.elo@nokia.com>
2022-02-10 17:19:39 +0200

    ci: replace travis ci with github actions
    Replace Travis CI with GitHub Actions due to Travis CI pricing policy
    changes.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,16 +2,20 @@
 
 ROOTDIR=$(git rev-parse --show-toplevel)
 
+if [ "${CC#clang}" != "${CC}" ] ; then
+	export CXX="clang++"
+fi
+
 cd $ROOTDIR
-git clone --depth 1 -b v1.19.0.1 https://github.com/Linaro/odp.git
+git clone --depth 1 -b v1.19.0.1 https://github.com/OpenDataPlane/odp.git
 cd odp
 ./bootstrap
 ODPDIR=$(pwd)/install
 ./configure --prefix=$ODPDIR --enable-deprecated
-make -j 4 install
+make -j $(nproc) install
 
 cd $ROOTDIR
 ./bootstrap
 ./configure --with-odp=$ODPDIR --enable-cunit --prefix=$(pwd)/install
-make install
+make -j $(nproc) install
 make check
