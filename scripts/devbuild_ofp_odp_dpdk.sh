cf466c060d182e68b48821f860d31df77fe6054a
Jere Leppänen <jere.leppanen@nokia.com>
2022-08-05 13:14:33 +0300

    scripts: devbuild_ofp_odp_dpdk.sh: Fix dpdk PKG_CONFIG_PATH
    Also make some other minor changes.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- scripts/devbuild_ofp_odp_dpdk.sh
+++ scripts/devbuild_ofp_odp_dpdk.sh
@@ -2,6 +2,10 @@
 
 JOBS=${JOBS:-$(nproc)}
 
+if [ "${CC#clang}" != "${CC}" ] ; then
+	export CXX="clang++"
+fi
+
 cd $(readlink -e $(dirname $0))/..
 
 # Build DPDK
@@ -15,14 +19,14 @@ popd
 popd
 
 # Build ODP
-git clone https://github.com/OpenDataPlane/odp-dpdk --branch v1.35.0.0_DPDK_19.11 --depth 1 ./odp-dpdk
+git clone https://github.com/OpenDataPlane/odp-dpdk --branch v1.35.0.0_DPDK_19.11 --depth 1
 pushd odp-dpdk
 ./bootstrap
-PKG_CONFIG_PATH=$(pwd)/../dpdk/install/lib/x86_64-linux-gnu/pkgconfig ./configure --prefix=$(pwd)/install
+PKG_CONFIG_PATH=$(pwd)/../dpdk/install/lib64/pkgconfig:${PKG_CONFIG_PATH} ./configure --prefix=$(pwd)/install
 make -j${JOBS} install
 popd
 
 # Build OFP
 ./bootstrap
-./configure --with-odp-lib=odp-dpdk --with-odp=$(pwd)/odp-dpdk/install --prefix=$(pwd)/install
+./configure --with-odp-lib=odp-dpdk --with-odp=$(pwd)/odp-dpdk/install --prefix=$(pwd)/install --enable-cunit
 make -j${JOBS} install
