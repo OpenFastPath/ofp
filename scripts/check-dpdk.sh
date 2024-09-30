cc5226f669acccb969218ab9205370d41d78d971
Jere Leppänen <jere.leppanen@nokia.com>
2024-09-30 11:44:22 +0300

    scripts: check-dpdk.sh: set up hugepages after compilation
    Set up hugepages after compilation, so that the script can be run up
    to that point as a normal user.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- scripts/check-dpdk.sh
+++ scripts/check-dpdk.sh
@@ -8,11 +8,6 @@ fi
 
 cd $(readlink -e $(dirname $0))/..
 
-# Configure hugepages
-sysctl vm.nr_hugepages=1000
-mkdir -p /mnt/huge
-mount -t hugetlbfs nodev /mnt/huge
-
 # Build ODP
 git clone https://github.com/OpenDataPlane/odp-dpdk --branch v1.41.0.0_DPDK_22.11 --depth 1
 pushd odp-dpdk
@@ -25,6 +20,11 @@ popd
 ./bootstrap
 ./configure --with-odp-lib=odp-dpdk --with-odp=$(pwd)/odp-dpdk/install --prefix=$(pwd)/install --enable-cunit
 make -j${JOBS} install
+
+# Configure hugepages
+sysctl vm.nr_hugepages=1000
+mkdir -p /mnt/huge
+mount -t hugetlbfs nodev /mnt/huge
 
 # Test OFP
 make check
