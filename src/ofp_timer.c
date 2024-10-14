OpenFastPath/ofp

commit db2601fe7610311fe13a5f93cd7fa4a05c60f5d6

timer: Use odp_timer_start() instead of odp_timer_set_abs()
odp_timer_set_abs() has been deprecated, use odp_timer_start()
instead.

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Matias Elo <matias.elo@nokia.com>

src/ofp_timer.c
@@ -427,13 +427,18 @@ odp_timer_t ofp_timer_start_cpu_id(uint64_t tmo_us, ofp_timer_callback callback,
 			return ODP_TIMER_INVALID;
 		}
 
-		t = odp_timer_set_abs(timer, tick, &bufdata->t_ev);
+		odp_timer_start_t param;
+
+		param.tick_type = ODP_TIMER_TICK_ABS;
+		param.tick = tick;
+		param.tmo_ev = bufdata->t_ev;
+		t = odp_timer_start(timer, &param);
 
 		if (t != ODP_TIMER_SUCCESS) {
 			odp_timer_free(timer);
 			odp_timeout_free(tmo);
 			odp_buffer_free(buf);
-			OFP_ERR("odp_timer_set_abs failed");
+			OFP_ERR("odp_timer_start failed");
 			return ODP_TIMER_INVALID;
 		}
 

commit addd8a36fbd318ac05784c7aaf96df9e9ec9b22e

timer: don't use odp_timer_set_t
odp_timer_set_t has been removed, don't use it. This also fixes a
mistake made in an earlier commit, where the return value of
odp_timer_start(), which is int, was assigned to a variable of type
odp_timer_set_t.

Fixes: db2601fe7610 ("timer: Use odp_timer_start() instead of odp_timer_set_abs()")

Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

src/ofp_timer.c
@@ -356,7 +356,6 @@ odp_timer_t ofp_timer_start_cpu_id(uint64_t tmo_us, ofp_timer_callback callback,
 	uint64_t period_ns;
 	struct ofp_timer_internal *bufdata;
 	odp_buffer_t buf;
-	odp_timer_set_t t;
 	odp_timeout_t tmo;
 
 	/* Init shm if not done yet. */
@@ -432,9 +431,8 @@ odp_timer_t ofp_timer_start_cpu_id(uint64_t tmo_us, ofp_timer_callback callback,
 		param.tick_type = ODP_TIMER_TICK_ABS;
 		param.tick = tick;
 		param.tmo_ev = bufdata->t_ev;
-		t = odp_timer_start(timer, &param);
 
-		if (t != ODP_TIMER_SUCCESS) {
+		if (odp_timer_start(timer, &param) != ODP_TIMER_SUCCESS) {
 			odp_timer_free(timer);
 			odp_timeout_free(tmo);
 			odp_buffer_free(buf);